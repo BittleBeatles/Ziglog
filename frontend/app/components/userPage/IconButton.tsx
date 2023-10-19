97301aa20fa1bf27c3bcb6acc76147adf4624d1c
hanulkimm <hanulkim2000@naver.com>
2023-10-19 23:54:31 +0900

    feat: IconButton Component 추가
    user-page에 쓰이는 아이콘 버튼들

---
+++ frontend/app/components/userPage/IconButton.tsx
@@ -0,0 +1,14 @@
+import { ButtonHTMLAttributes } from 'react';
+import SvgIcon from '@components/common/SvgIcon';
+import * as Icons from '../../src/design/iconIndex';
+
+interface IconButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  name: keyof typeof Icons;
+}
+export default function IconButton({ name }: IconButtonProps) {
+  return (
+    <div className=" bg-white w-fit h-fit p-1 rounded-lg border-2 border-border-grey">
+      <SvgIcon name={name} />
+    </div>
+  );
+}
