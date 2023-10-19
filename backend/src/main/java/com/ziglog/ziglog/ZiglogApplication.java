9b3584b9364e25eb56b2bc94cb8e70313d55f024
박영서 <pj0642@gmail.com>
2023-10-19 12:43:14 +0900

    init: 백엔드 서버 초기 생성

---
+++ backend/src/main/java/com/ziglog/ziglog/ZiglogApplication.java
@@ -0,0 +1,13 @@
+package com.ziglog.ziglog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZiglogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZiglogApplication.class, args);
+	}
+
+}
