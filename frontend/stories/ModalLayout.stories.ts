51184db80bbaa14966838f385e6fcad9d7ce0a9a
Hanul Kim <hanulkim2000@korea.ac.kr>
2023-10-19 16:21:22 +0900

    fix:  modalLayout 스토리북 수정

---
+++ frontend/stories/ModalLayout.stories.ts
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import ModalLayout from '@components/common/ModalLayout';
+
+const meta: Meta<typeof ModalLayout> = {
+  component: ModalLayout,
+} satisfies Meta<typeof ModalLayout>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const ModalLayoutExample = {
+  args: {
+    children: '모달 예시입니다.',
+    classname: '',
+  },
+} satisfies Story;
