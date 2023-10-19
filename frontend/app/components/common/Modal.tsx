6b761de011a99cb02cebb6eea2b3701cb1705d58
Hanul Kim <hanulkim2000@korea.ac.kr>
2023-10-19 13:40:06 +0900

    feat: add Modal component

---
+++ frontend/app/components/common/Modal.tsx
@@ -0,0 +1,12 @@
+'use client';
+import { useState } from 'react';
+
+export default function Modal() {
+  const [isOpen, setIsOpen] = useState(true);
+  function closeModal() {
+    setIsOpen(false)
+  }
+  return(
+    
+  )
+}
