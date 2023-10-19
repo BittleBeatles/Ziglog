7081b631e9959f0c1a75b80ad23b775ee5c24728
hanulkimm <hanulkim2000@naver.com>
2023-10-20 00:26:42 +0900

    feat: 공개/비공개 토글 버튼 Storybook 추가

---
+++ frontend/stories/PublicPrivateButton.stories.ts
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import PublicPrivateToggle from '@components/userPage/PublicPrivateToggle';
+
+const meta: Meta<typeof PublicPrivateToggle> = {
+  component: PublicPrivateToggle,
+} satisfies Meta<typeof PublicPrivateToggle>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const PublicPrivateToggleExample = {
+  args: {
+    type: 'private',
+  },
+} satisfies Story;
