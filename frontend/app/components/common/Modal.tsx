3882b9aadee42de953bb059ffdab24fdbce0a588
Hanul Kim <hanulkim2000@korea.ac.kr>
2023-10-19 15:50:21 +0900

    design: Modal 기본 css 추가

--- frontend/app/components/common/Modal.tsx
+++ frontend/app/components/common/Modal.tsx
@@ -1,12 +1,17 @@
-'use client';
-import { useState } from 'react';
-
-export default function Modal() {
-  const [isOpen, setIsOpen] = useState(true);
-  function closeModal() {
-    setIsOpen(false)
-  }
-  return(
-    
-  )
+import { ReactNode } from 'react';
+interface ModalProps {
+  children: ReactNode;
+  size: 'small' | 'large';
 }
+export default function Modal({ children, size }: ModalProps) {
+  return (
+    <div className={`${SIZE_VARIANTS[size]} bg-modal shadow-md`}>
+      {children}
+    </div>
+  );
+}
+
+const SIZE_VARIANTS = {
+  small: 'w-96 h-60',
+  large: 'w-1/3 h-96',
+};
