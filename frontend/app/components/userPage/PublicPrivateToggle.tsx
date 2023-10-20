b1448aae6d79701390fafd90997a33e9a7aba2c5
Hanul Kim <hanulkim2000@korea.ac.kr>
2023-10-20 09:42:41 +0900

    fix : 비공개/공개 span 태그를 text 태그로 변경

--- frontend/app/components/userPage/PublicPrivateToggle.tsx
+++ frontend/app/components/userPage/PublicPrivateToggle.tsx
@@ -1,5 +1,7 @@
 import { ButtonHTMLAttributes } from 'react';
 import SvgIcon from '@components/common/SvgIcon';
+import Text from '@components/common/Text';
+
 interface PublicPrivateToggleProps
   extends ButtonHTMLAttributes<HTMLButtonElement> {
   type: 'public' | 'private';
@@ -11,7 +13,7 @@ export default function PublicPrivateToggle({
   return (
     <button className="flex flex-row gap-3 bg-light-background-layout border border-border-grey rounded-lg w-fit p-3 font-bold">
       <SvgIcon name={type} />
-      <span>{type === 'public' ? '전체공개' : '비공개'}</span>
+      <Text type="span">{type === 'public' ? '전체공개' : '비공개'}</Text>
     </button>
   );
 }
