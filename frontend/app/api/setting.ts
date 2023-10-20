5c8166bdfc000d1cb9856a1ef6c94da88fd66326
yongseong2 <ancjs369@naver.com>
2023-10-20 16:38:22 +0900

    chore: api/ index setting 분리

---
+++ frontend/app/api/setting.ts
@@ -0,0 +1,59 @@
+import returnFetch, {
+  FetchArgs,
+  ReturnFetchDefaultOptions,
+} from 'return-fetch';
+
+type JsonRequestInit = Omit<NonNullable<FetchArgs[1]>, 'body'> & {
+  body?: object;
+};
+
+export type ResponseGenericBody<T> = Omit<
+  Awaited<ReturnType<typeof fetch>>,
+  keyof Body | 'clone'
+> & {
+  body: T;
+};
+
+export type JsonResponse<T> = T extends object
+  ? ResponseGenericBody<T>
+  : ResponseGenericBody<unknown>;
+
+const parseJsonSafely = (text: string): object | string => {
+  try {
+    return JSON.parse(text);
+  } catch (e) {
+    if ((e as Error).name !== 'SyntaxError') {
+      throw e;
+    }
+
+    return text.trim();
+  }
+};
+
+// axios를 대체할 fetch 함수
+export const returnFetchJson = (args?: ReturnFetchDefaultOptions) => {
+  const fetch = returnFetch(args);
+
+  return async <T>(
+    url: FetchArgs[0],
+    init?: JsonRequestInit
+  ): Promise<JsonResponse<T>> => {
+    const response = await fetch(url, {
+      ...init,
+      body: init?.body && JSON.stringify(init.body),
+    });
+
+    const body = parseJsonSafely(await response.text()) as T;
+
+    return {
+      headers: response.headers,
+      ok: response.ok,
+      redirected: response.redirected,
+      status: response.status,
+      statusText: response.statusText,
+      type: response.type,
+      url: response.url,
+      body,
+    } as JsonResponse<T>;
+  };
+};
