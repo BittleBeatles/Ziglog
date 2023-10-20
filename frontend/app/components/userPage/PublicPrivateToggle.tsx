52d1dab81932ca17418bf41b9be3a2ead0a8f3c5
Hanul Kim <hanulkim2000@korea.ac.kr>
2023-10-20 09:38:34 +0900

    fix: 비공개/공개 토글 버튼 button으로 수정

--- frontend/app/components/userPage/PublicPrivateToggle.tsx
+++ frontend/app/components/userPage/PublicPrivateToggle.tsx
@@ -9,9 +9,9 @@ export default function PublicPrivateToggle({
   type,
 }: PublicPrivateToggleProps) {
   return (
-    <div className="flex flex-row gap-3 bg-light-background-layout border border-border-grey rounded-lg w-fit p-3 font-bold">
+    <button className="flex flex-row gap-3 bg-light-background-layout border border-border-grey rounded-lg w-fit p-3 font-bold">
       <SvgIcon name={type} />
       <span>{type === 'public' ? '전체공개' : '비공개'}</span>
-    </div>
+    </button>
   );
 }
