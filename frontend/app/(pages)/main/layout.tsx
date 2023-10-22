313d663a057ed488ffa1a8f95cbc17f53f22367d
ababwjdgusdk <ababwjdgusdk@naver.com>
2023-10-22 22:25:51 +0900

    fix: 네브바입니다 text 삭제

--- frontend/app/(pages)/main/layout.tsx
+++ frontend/app/(pages)/main/layout.tsx
@@ -4,7 +4,6 @@ import { ReactNode } from 'react';
 export default function MainLayout({ children }: { children: ReactNode }) {
   return (
     <div>
-      <h1>메인 네브바 입니다</h1>
       <NavBar login={false} />
       {children}
     </div>
