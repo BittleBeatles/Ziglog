8b08cee6898fcf68c3301716c94e79d0e56503ce
ababwjdgusdk <ababwjdgusdk@naver.com>
2023-10-22 21:27:26 +0900

    feat: NavBar 기초 다크모드,검색 아이콘 및 로그인버튼 추가

---
+++ frontend/app/components/common/NavBar.tsx
@@ -0,0 +1,32 @@
+import { HTMLAttributes } from 'react';
+import IconButton from '@components/userPage/IconButton';
+import Button from '@components/common/Button';
+
+interface NavBarProps extends HTMLAttributes<HTMLDivElement> {
+  login: boolean;
+  theme?: 'light' | 'dark';
+  type: 'lightMode' | 'darkMode';
+}
+
+export default function NavBar({
+  login,
+  theme = 'light',
+  type = 'lightMode',
+  ...rest
+}: NavBarProps) {
+  return (
+    <div
+      {...rest}
+      className={`${THEME_VARIANTS[theme]} w-full h-full shadow-md p-5`}
+    >
+      <IconButton name={type === 'lightMode' ? 'lightMode' : 'darkMode'} />
+      <IconButton name="search" />
+      <Button label="로그인" color="charcol" />
+    </div>
+  );
+}
+
+const THEME_VARIANTS = {
+  light: 'bg-white text-black shadow-dark-500/50',
+  dark: 'bg-dark-background-page text-white shadow-white-500/50',
+};
