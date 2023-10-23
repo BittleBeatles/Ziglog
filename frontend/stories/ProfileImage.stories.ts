d032151d546cf17009e2b0e1bc258dafacb28001
emmajane <emmajane@naver.com>
2023-10-23 10:04:56 +0900

    feat: Profile Image 구현

---
+++ frontend/stories/ProfileImage.stories.ts
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import ProfileImage from '@components/common/ProfileImage';
+
+const meta: Meta<typeof ProfileImage> = {
+  component: ProfileImage,
+} satisfies Meta<typeof ProfileImage>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const ProfileImageExample = {
+  args: {
+    size: 50,
+  },
+} satisfies Story;
