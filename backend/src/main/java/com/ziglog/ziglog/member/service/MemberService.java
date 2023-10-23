7bfd0871839d0e7ac6bf06df10da667a6b976c02
박영서 <pj0642@gmail.com>
2023-10-23 16:42:10 +0900

    feat: 회원 관리 서비스 정의 - 이메일을 통한 사용자 조회 - 닉네임을 통한 사용자 조회 - 닉네임 변경 - 닉네임 중복 확인 - 회원 가입

--- backend/src/main/java/com/ziglog/ziglog/member/service/MemberService.java
+++ backend/src/main/java/com/ziglog/ziglog/member/service/MemberService.java
@@ -1,10 +1,16 @@
 package com.ziglog.ziglog.member.service;
 
+import com.ziglog.ziglog.member.entity.Member;
+
+import java.util.Optional;
+
 public interface MemberService {
 
-    void signIn();
+    Optional<Member> findUserByEmail(String email);
+    Optional<Member> findUserByNickname(String nickname);
 
+    void modifyUserNickname(String nickname);
+    boolean checkNicknameDuplication(String nickname);
 
-
-
+    void signUp(String email, String nickname);
 }
