448748dd37d01b2a548d975de966522403a30012
Hanul Kim <hanulkim2000@korea.ac.kr>
2023-10-23 14:17:57 +0900

    chore: 프로필 수정 상위 페이지 예시 - 추후 삭제 예정

--- frontend/app/(pages)/main/page.tsx
+++ frontend/app/(pages)/main/page.tsx
@@ -1,9 +1,28 @@
+'use client';
 import ModalLayout from '@components/common/ModalLayout';
+import ProfileChangeButton from '@components/common/ProfileChangeButton';
+
+import { useRef } from 'react';
+
 export default function Main() {
+  const imageRef = useRef<HTMLInputElement>(null);
+  const handleProfileImageChangeClick = () => {
+    if (imageRef.current) {
+      imageRef.current.click();
+    }
+  };
+  const handleImageInput = () => {
+    console.log('[이미지 업로드 로직]');
+  };
   return (
     <div>
       <h1>메인페이지입니다.</h1>
       <ModalLayout>모달 예시입니다.</ModalLayout>
+      <ProfileChangeButton
+        onClick={handleProfileImageChangeClick}
+        onInput={handleImageInput}
+        ref={imageRef}
+      />
     </div>
   );
 }
