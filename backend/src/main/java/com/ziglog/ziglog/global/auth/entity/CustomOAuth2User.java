73c3552c695371494f83ecf82096a1aa79683c11
박영서 <pj0642@gmail.com>
2023-10-23 17:33:09 +0900

    feat: OAuth2User 정의 - Member에 @Getter 애노테이션 추가

---
+++ backend/src/main/java/com/ziglog/ziglog/global/auth/entity/CustomOAuth2User.java
@@ -0,0 +1,48 @@
+package com.ziglog.ziglog.global.auth.entity;
+
+import com.ziglog.ziglog.domain.member.entity.Member;
+import com.ziglog.ziglog.domain.member.entity.Role;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+
+import java.util.Collection;
+import java.util.Map;
+
+public class CustomOAuth2User extends DefaultOAuth2User {
+
+    private final Member member;
+
+    public CustomOAuth2User(Collection<? extends GrantedAuthority> authorities,
+                            Map<String, Object> attributes, String nameAttributeKey, Member member) {
+        super(authorities, attributes, nameAttributeKey);
+        this.member = member;
+    }
+
+    @Override
+    public String getName() {
+        return member.getNickname();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return super.getAuthorities();
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return super.getAttributes();
+    }
+
+    @Override
+    public <A> A getAttribute(String name) {
+        return super.getAttribute(name);
+    }
+
+    public Role getRole() {
+        return this.member.getRole();
+    }
+
+    public Member getMember() {
+        return this.member;
+    }
+}
