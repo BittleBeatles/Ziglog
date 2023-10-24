bbd798afac8df96a16133214cc09b9d8abf0b5a1
박영서 <pj0642@gmail.com>
2023-10-24 12:17:13 +0900

    feat: CustomOAuth2UserService에 loadUser() 메서드를 추가 + OAuth2Attributes에 of 메서드 추가하고 ofGoogle을 분리

--- backend/src/main/java/com/ziglog/ziglog/global/auth/service/CustomOAuth2UserService.java
+++ backend/src/main/java/com/ziglog/ziglog/global/auth/service/CustomOAuth2UserService.java
@@ -4,6 +4,7 @@ import com.ziglog.ziglog.domain.member.entity.Member;
 import com.ziglog.ziglog.domain.member.repository.MemberRepository;
 import com.ziglog.ziglog.global.auth.dto.ClientName;
 import com.ziglog.ziglog.global.auth.dto.OAuth2Attributes;
+import com.ziglog.ziglog.global.auth.entity.OAuth2UserInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
@@ -20,6 +21,19 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
+        OAuth2UserService<OAuth2UserRequest, OAuth2User> defaultOAuth2UserService = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = defaultOAuth2UserService.loadUser(userRequest);
+
+        ClientName clientName = ClientName.valueOf(userRequest.getClientRegistration().getRegistrationId());
+
+        String nameAttributeName = userRequest.getClientRegistration().getProviderDetails()
+                .getUserInfoEndpoint().getUserNameAttributeName();
+
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+
+        OAuth2Attributes oAuth2Attributes = OAuth2Attributes.of(clientName, nameAttributeName, attributes);
+
         return  null;
     }
 }
