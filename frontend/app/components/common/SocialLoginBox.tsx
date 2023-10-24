fd731e5a870786647e6fd0cf2cd59a3f460ea3dc
ababwjdgusdk <ababwjdgusdk@naver.com>
2023-10-24 22:54:06 +0900

    fix: merge 해결 및 메인페이지에서  소셜로그인박스 삭제

--- frontend/app/components/common/SocialLoginBox.tsx
+++ frontend/app/components/common/SocialLoginBox.tsx
@@ -8,12 +8,14 @@ interface SocialLoginBoxProps {
 
 export default function SocialLoginBox({ theme }: SocialLoginBoxProps) {
   return (
-    <ModalLayout classname={theme}>
+    <ModalLayout classname="px-24">
       {
-        <div className="">
-          <Text type="h2">{'소셜 계정으로 로그인 '}</Text>
-          <SocialLoginButton name="google" />
-          <SocialLoginButton name="kakao" />
+        <div className="flex flex-col items-center justify-center my-4">
+          <Text type="b">{'소셜 계정으로 로그인 '}</Text>
+          <div className="mt-4 flex flex-col items-center">
+            <SocialLoginButton name="google" className="mt-2 drop-shadow" />
+            <SocialLoginButton name="kakao" className="mt-5 drop-shadow" />
+          </div>
         </div>
       }
     </ModalLayout>
