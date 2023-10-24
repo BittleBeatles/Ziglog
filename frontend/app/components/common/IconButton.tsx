63b2821eafcf5140be78a7ca5caf16ed379ae9ee
Hanul Kim <hanulkim2000@korea.ac.kr>
2023-10-24 09:44:35 +0900

    fix: 배경없는 아이콘 버튼 common 으로 수정
    - 색깔, 사이즈 추가

--- frontend/app/components/common/IconButton.tsx
+++ frontend/app/components/common/IconButton.tsx
@@ -6,13 +6,20 @@ import colors from '@src/design/color';
 interface IconButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   name: keyof typeof Icons;
   theme: 'dark' | 'light';
+  size: number;
 }
-export default function IconButton({ name, theme, ...rest }: IconButtonProps) {
+export default function IconButton({
+  name,
+  theme,
+  size,
+  ...rest
+}: IconButtonProps) {
   return (
     <button {...rest}>
       <SvgIcon
         name={name}
         color={theme === 'dark' ? colors.white : colors.black}
+        size={size}
       />
     </button>
   );
--- frontend/stories/IconButton.stories.ts
+++ frontend/stories/IconButton.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import IconButton from '@components/userPage/IconButton';
+import IconButton from '@components/common/IconButton';
 
 const meta: Meta<typeof IconButton> = {
   component: IconButton,
