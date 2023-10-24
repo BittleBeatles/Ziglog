4a1338625b22725891629d569c3eaa713fd68416
박영서 <pj0642@gmail.com>
2023-10-24 23:25:38 +0900

    feat: OAuth2LoginSuccessHandler 구현 - OAuth2 로그인 성공 시 -> {frontUrl}/oauth2/redirect/{accessToken}으로 리다이렉트 함 - url이 아니라 body에 담아서 주도록 바꿀 수도 있으니 상의 필요 - 받은 access token으로 서버에 재요청하면 유저 정보를 줄 수 있음.

--- backend/src/main/java/com/ziglog/ziglog/global/auth/handler/OAuth2LoginSuccessHandler.java
+++ backend/src/main/java/com/ziglog/ziglog/global/auth/handler/OAuth2LoginSuccessHandler.java
@@ -1,21 +1,62 @@
 package com.ziglog.ziglog.global.auth.handler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ziglog.ziglog.domain.member.repository.MemberRepository;
+import com.ziglog.ziglog.global.auth.entity.oauth2.CustomOAuth2User;
+import com.ziglog.ziglog.global.auth.service.CustomOAuth2UserService;
+import com.ziglog.ziglog.global.auth.service.JwtService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
+
+    private final JwtService jwtService;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final MemberRepository memberRepository;
+
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
+    private String frontUrl = "localhost:3000";
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        log.info("OAuth2LoginSuccessHandler - onAuthenticationSuccess()");
+        try {
+            log.info("OAuth2LoginSuccessHandler - onAuthenticationSuccess()");
+            CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
+            loginSuccess(request, response, oAuth2User);
+        } catch (Exception e){
+            log.info("OAuth2LoginSuccessHandler - onAuthenticationSuccess() fails : {}", e.getMessage());
+        }
+    }
 
+    private void loginSuccess(HttpServletRequest request, HttpServletResponse response, CustomOAuth2User oAuth2User) throws IOException {
+        if (response.isCommitted()) return;
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+
+        String accessToken = jwtService.issueAccessToken(oAuth2User.getName());
+        String refreshToken = jwtService.issueRefreshToken();
+
+        String redirectUrl = UriComponentsBuilder.fromUriString(frontUrl+"/oauth2/redirect/" + accessToken)
+                .build().toUriString();
+
+        jwtService.sendAccessTokenAndRefreshToken(response, accessToken, refreshToken);
+        jwtService.saveRefreshToken(refreshToken, oAuth2User.getMember());
+        redirectStrategy.sendRedirect(request, response, redirectUrl);
     }
 }
