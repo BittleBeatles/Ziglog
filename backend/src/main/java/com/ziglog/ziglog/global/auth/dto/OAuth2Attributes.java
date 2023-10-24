a9cfbd15cfe4ed6ba9ec19305427a58741230729
박영서 <pj0642@gmail.com>
2023-10-24 12:06:00 +0900

    feat: OAuth2 DTO 및 entity 추가 ## Domain - Member ###  Entity - Member 엔티티에 닉네임 Setter 추가 ## Global - Auth ### Entity - CustomOAuth2User : OAuth2에서 사용될 사용자 엔티티 - OAuth2UserInfo : Attribute를 가지는 엔티티. 사용자의 개인 정보 조회용 - OAuth2GoogleUserInfo : Google에서 제공하는 정보에 맞게 OAuth2UserInfo 확장 ### DTO - OAuth2Attributes : Client 종류에 따라 OAuth2UserInfo를 다르게 만들기 위한 클래스 - ClientName : 인증 대리 클라이언트 이름들의 enum

--- backend/src/main/java/com/ziglog/ziglog/global/auth/dto/OAuth2Attributes.java
+++ backend/src/main/java/com/ziglog/ziglog/global/auth/dto/OAuth2Attributes.java
@@ -1,4 +1,30 @@
 package com.ziglog.ziglog.global.auth.dto;
 
+import com.ziglog.ziglog.global.auth.entity.OAuth2GoogleUserInfo;
+import com.ziglog.ziglog.global.auth.entity.OAuth2UserInfo;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
 public class OAuth2Attributes {
+
+    private String nameAttributeKey;
+    private OAuth2UserInfo oAuth2UserInfo;
+
+    @Builder
+    public OAuth2Attributes(String nameAttributeKey, OAuth2UserInfo oAuth2UserInfo){
+        this.nameAttributeKey = nameAttributeKey;
+        this.oAuth2UserInfo = oAuth2UserInfo;
+    }
+
+    public static OAuth2Attributes ofGoogle(String nameAttributeKey, Map<String, Object> attributes){
+        return OAuth2Attributes.builder()
+                .nameAttributeKey(nameAttributeKey)
+                .oAuth2UserInfo(new OAuth2GoogleUserInfo(attributes))
+                .build();
+    }
+
+
 }
--- backend/src/main/java/com/ziglog/ziglog/global/auth/entity/OAuth2GoogleUserInfo.java
+++ backend/src/main/java/com/ziglog/ziglog/global/auth/entity/OAuth2GoogleUserInfo.java
@@ -1,4 +1,22 @@
 package com.ziglog.ziglog.global.auth.entity;
 
-public class OAuth2GoogleUserInfo {
+import com.ziglog.ziglog.global.auth.dto.OAuth2Attributes;
+
+import java.util.Map;
+
+public class OAuth2GoogleUserInfo extends OAuth2UserInfo {
+
+    public OAuth2GoogleUserInfo(Map<String, Object> attributes) {
+        super(attributes);
+    }
+
+    @Override
+    public String getEmail() {
+        return (String) attributes.get("email");
+    }
+
+    @Override
+    public String getNickname() {
+        return (String) attributes.get("nickname");
+    }
 }
