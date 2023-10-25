bc87d65f5447f809e0e9e56756bbc144c2a96ea3
ababwjdgusdk <ababwjdgusdk@naver.com>
2023-10-25 15:54:36 +0900

    design: socialloginbox css 수정

--- frontend/app/components/common/SocialLoginBox.tsx
+++ frontend/app/components/common/SocialLoginBox.tsx
@@ -10,13 +10,11 @@ interface SocialLoginBoxProps {
 
 export default function SocialLoginBox({ theme }: SocialLoginBoxProps) {
   return (
-    <ModalLayout classname={`${THEME_VARIANTS[theme]}`}>
-      <div className="flex flex-col justify-center items-center my-4 mx-14">
+    <ModalLayout classname={`${THEME_VARIANTS[theme]} px-20`}>
+      <div className="flex flex-col justify-center items-center my-4 gap-4">
         <Text type="b">{'소셜 계정으로 로그인 '}</Text>
-        <div className="flex flex-col gap-3 mt-3">
-          <SocialLoginButton name="google" />
-          <SocialLoginButton name="kakao" />
-        </div>
+        <SocialLoginButton name="google" />
+        <SocialLoginButton name="kakao" />
       </div>
     </ModalLayout>
   );
