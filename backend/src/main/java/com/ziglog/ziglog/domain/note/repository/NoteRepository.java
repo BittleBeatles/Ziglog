d2e13a011ee380c9be7f5cb84ca3c37517461611
박영서 <pj0642@gmail.com>
2023-10-25 23:55:32 +0900

    feat: Folder, Note, Quotation Repository에 필요한 것들을 정의 - 이후에 JPQL이든 QueryDSL이든 써서 최적화할 예정임

--- backend/src/main/java/com/ziglog/ziglog/domain/note/repository/NoteRepository.java
+++ backend/src/main/java/com/ziglog/ziglog/domain/note/repository/NoteRepository.java
@@ -3,5 +3,13 @@ package com.ziglog.ziglog.domain.note.repository;
 import com.ziglog.ziglog.domain.note.entity.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface NoteRepository extends JpaRepository<Note, Long> {
+
+    public Note findNoteById(Long noteId);
+
+    public List<Note> findAllById(Long noteId);
+
+    public void removeNoteById(Long noteId);
 }
