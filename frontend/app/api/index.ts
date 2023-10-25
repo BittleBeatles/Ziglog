e2904769a17fdd18c6fd6d38aec6fc7f25589fe8
Hanul Kim <hanulkim2000@korea.ac.kr>
2023-10-25 15:56:58 +0900

    feat: axios header 추가

--- frontend/app/api/index.ts
+++ frontend/app/api/index.ts
@@ -4,9 +4,20 @@ const API_URL = '';
 // fetch 기본 설정 이에요 ex) baseUrl, interceptor 추가가능
 export const publicFetch = returnFetchJson({
   baseUrl: API_URL,
+  headers: {
+    'Access-Control-Allow-Origin': 'http://localhost:3000',
+    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+    'Content-Type': 'application/json',
+  },
 });
 
 export const privateFetch = returnFetchJson({
   baseUrl: API_URL,
+  headers: {
+    'Access-Control-Allow-Origin': 'http://localhost:3000',
+    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+    'Content-Type': 'application/json',
+    // 'Access-Control-Credentials': true,
+  },
   interceptors: {},
 });
