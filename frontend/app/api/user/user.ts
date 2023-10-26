f24b310d774e7b82fafe5707444bee8a374a2186
Hanul Kim <hanulkim2000@korea.ac.kr>
2023-10-26 15:07:45 +0900

    feat: 개인정보 요청 fetch 추가

---
+++ frontend/app/api/user/user.ts
@@ -0,0 +1,14 @@
+import { privateFetch, publicFetch } from '..';
+import { API_URL } from '@api/constants';
+import { UserInfo } from './types';
+export function getUserInfo(): Promise<UserInfo> {
+  return privateFetch<UserInfo>(`${API_URL}/auth/oauth2/user-info`, {
+    method: 'GET',
+  })
+    .then((response) => {
+      return response.body;
+    })
+    .catch((error) => {
+      throw error;
+    });
+}
