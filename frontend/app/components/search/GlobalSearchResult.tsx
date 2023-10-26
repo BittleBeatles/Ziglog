98a664e13f7cfbd6b1a976b5bc34882bb0438aa9
ababwjdgusdk <ababwjdgusdk@naver.com>
2023-10-26 13:22:38 +0900

    feat: 글검색 결과 컴포넌트 UI 만듦

--- frontend/app/components/search/GlobalSearchResult.tsx
+++ frontend/app/components/search/GlobalSearchResult.tsx
@@ -8,7 +8,7 @@ interface GlobalSearchResultProps {
   nickname: string;
   is_public: 1;
   bookmark_count: number;
-  saved_time: string;
+  post_time: string;
   theme: 'light' | 'dark';
 }
 
@@ -18,16 +18,34 @@ export default function GlobalSearchResult({
   content,
   nickname,
   is_public = 1,
-  bookmark_count,
-  saved_time,
+  bookmark_count = 10,
+  post_time,
   theme,
   ...rest
 }: GlobalSearchResultProps) {
   return (
-    <div {...rest} className={`${THEME_VARIANTS[theme]}`}>
-      <div className="flex items-center">
+    <div {...rest} className={`${THEME_VARIANTS[theme]} p-5`}>
+      <div className="flex flex-row items-center">
         <ProfileImage size={40} />
-        <Text>{'seongyong2'}</Text>
+        <Text className="ml-12">{'seongyong2'}</Text>
+      </div>
+      <div className="mt-5">
+        <Text type="h3">{'노션 같은 APP 만들기'}</Text>
+      </div>
+      <div className="mt-2">
+        <Text type="p" className="line-clamp-2">
+          {
+            '코드실행 결과리스트 테이블 뷰에서 항목을 선택하면 나오는 뷰를 디자인하였습니다. Table View (제약 조건을 주었습니다) Table View CellImage ViewLableButtonFile->New->Cocoa Touch Class -> Clas 이름 작성을 하여 코드실행 결과리스트 테이블 뷰에서 항목을 선택하면 나오는 뷰를 디자인하였습니다. Table View (제약 조건을 주었습니다) Table View CellImage ViewLableButtonFile->New->Cocoa Touch Class -> Clas 이름 작성을 하여'
+          }
+        </Text>
+      </div>
+      <div className="mt-1 flex flex-row">
+        <Text type="p" className="text-xs text-grey ">
+          {'2023년 10월 26일'}
+        </Text>
+        <Text type="p" className="ml-2 text-xs text-grey">
+          {`북마크 ${bookmark_count}`}
+        </Text>
       </div>
     </div>
   );
