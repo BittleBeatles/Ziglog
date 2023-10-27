d16279f005efe4127a86a3e3d49a88efbf6e6590
박영서 <pj0642@gmail.com>
2023-10-27 14:37:13 +0900

    fix: CORS allowed origin *은 허용되지 않음

--- backend/src/main/java/com/ziglog/ziglog/global/auth/config/SecurityConfig.java
+++ backend/src/main/java/com/ziglog/ziglog/global/auth/config/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration corsConfig = new CorsConfiguration();
 
-        corsConfig.addAllowedOrigin("*");
+        corsConfig.addAllowedOrigin("http://localhost:3000");
         corsConfig.setAllowCredentials(true);
         corsConfig.addAllowedHeader("*");
         corsConfig.addAllowedMethod("*");
