cfcf06affbcb034e60b141809d1428df7790dd02
Hanul Kim <hanulkim2000@korea.ac.kr>
2023-10-27 15:14:32 +0900

    fix: memoization 제거
    - nextjs fetch의 default 는 cache-force

--- frontend/app/api/user/user.ts
+++ frontend/app/api/user/user.ts
@@ -3,7 +3,7 @@ import { API_URL } from '@api/constants';
 import { UserInfo } from './types';
 import { useAppDispatch } from '@store/store';
 import { setAccessToken } from '@store/modules/userSlice';
-import mem from 'mem';
+
 // promise Type 수정해야함
 export function getUserInfo(): Promise<UserInfo> {
   return privateFetch<UserInfo>(`${API_URL}/user/info`, {
@@ -17,26 +17,19 @@ export function getUserInfo(): Promise<UserInfo> {
     });
 }
 
-const cachedReissueToken = mem(
-  async function ReissueToken() {
-    const dispatch = useAppDispatch();
-    return privateFetch(`${API_URL}/auth/refresh`, {
-      method: 'GET',
-    })
-      .then((res) => {
-        dispatch(setAccessToken('newAccessToken'));
-        return Promise.resolve(res.body);
-      })
-      .catch((err) => {
-        if (!err.response.body || !err.config) {
-          console.log('[token reissue]:Unknown error');
-        }
-        return Promise.reject(err);
-      });
-  },
-  { maxAge: 5000 }
-);
-
 export async function ReissueToken() {
-  return cachedReissueToken();
+  const dispatch = useAppDispatch();
+  return privateFetch(`${API_URL}/auth/refresh`, {
+    method: 'GET',
+  })
+    .then((res) => {
+      dispatch(setAccessToken('newAccessToken'));
+      return Promise.resolve(res.body);
+    })
+    .catch((err) => {
+      if (!err.response.body || !err.config) {
+        console.log('[token reissue]:Unknown error');
+      }
+      return Promise.reject(err);
+    });
 }
