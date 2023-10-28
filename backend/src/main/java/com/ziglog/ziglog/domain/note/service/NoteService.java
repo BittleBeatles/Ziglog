2844c3cb88181a7a0944a6a2411f098bf1339490
박영서 <pj0642@gmail.com>
2023-10-28 19:10:52 +0900

    feat: NoteSevice 구현 수정 - 소유자 확인 로직 추가

--- backend/src/main/java/com/ziglog/ziglog/domain/note/service/NoteService.java
+++ backend/src/main/java/com/ziglog/ziglog/domain/note/service/NoteService.java
@@ -17,18 +17,18 @@ public interface NoteService {
 
     Note setPublic(Member member, Note note) throws Exception;
 
-    Boolean deleteNote(Member member, Long noteId); //해당 아이디의 노트를 삭제
+    Boolean deleteNote(Member member, Long noteId) throws Exception; //해당 아이디의 노트를 삭제
 
     Note getNote(Long noteId) throws Exception; //해당 ID의 노트의 Detail을 가져 오기
 
-    List<Note> findNotesQuotingThisNote(Long noteId); //이 노트를 참조하고 있는 노트의 리스트를 반환
+    List<Note> findNotesQuotingThisNote(Long noteId) throws Exception; //이 노트를 참조하고 있는 노트의 리스트를 반환
 
     // 폴더
     Folder addFolder(Member member, Folder folder);//새로운 폴더를 추가
 
     Folder modifyFolder(Member member, Folder folder) throws Exception;//폴더의 이름을 변경
 
-    Boolean deleteFolder(Member member, Long folderId); //해당 id의 폴더를 삭제
+    Boolean deleteFolder(Member member, Long folderId) throws Exception; //해당 id의 폴더를 삭제
 
     List<Folder> listFolder(String nickname) throws Exception; //해당 사용자의 모든 디렉토리를 반환
 
