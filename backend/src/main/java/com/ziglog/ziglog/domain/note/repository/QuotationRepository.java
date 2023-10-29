2b244d3170126d61100760fc3b562c4983606d36
박영서 <pj0642@gmail.com>
2023-10-29 17:36:06 +0900

    test: 노트 수정 테스트 - 이 글이 인용하고 있는 글 목록 업데이트 테스트

--- backend/src/main/java/com/ziglog/ziglog/domain/note/repository/QuotationRepository.java
+++ backend/src/main/java/com/ziglog/ziglog/domain/note/repository/QuotationRepository.java
@@ -4,10 +4,11 @@ import com.ziglog.ziglog.domain.note.entity.Note;
 import com.ziglog.ziglog.domain.note.entity.Quotation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuotationRepository extends JpaRepository<Quotation, Long> {
 
     void deleteQuotationById(Long id) throws Exception;
-
     Quotation findByStartNoteAndEndNote (Note startNote, Note endNote);
-
+    void deleteQuotationsByIdIn(List<Long> noteId);
 }
