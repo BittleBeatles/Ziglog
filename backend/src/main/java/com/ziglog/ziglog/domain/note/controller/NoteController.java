0f5f407b1ae41bba6df208025049d9d894a1a609
박영서 <pj0642@gmail.com>
2023-10-29 21:07:26 +0900

    feat: 이 글을 참조한 글 목록 DTO 작성

---
+++ backend/src/main/java/com/ziglog/ziglog/domain/note/controller/NoteController.java
@@ -0,0 +1,13 @@
+package com.ziglog.ziglog.domain.note.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/note")
+public class NoteController {
+
+
+}
