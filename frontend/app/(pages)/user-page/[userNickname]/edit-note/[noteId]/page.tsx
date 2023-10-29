01e683d61a106309952b68855cdb72b422670686
hanulkimm <hanulkim2000@naver.com>
2023-10-29 22:59:16 +0900

    feat: title input 연결, 위치 재배열
    - 공개/비공개 버튼과 저장/게시하기 버튼은 우측에

--- frontend/app/(pages)/user-page/[userNickname]/edit-note/[noteId]/page.tsx
+++ frontend/app/(pages)/user-page/[userNickname]/edit-note/[noteId]/page.tsx
@@ -1,28 +1,34 @@
 'use client';
 import { useParams } from 'next/navigation';
-import Text from '@components/common/Text';
 import MarkdownEditor from '@components/userPage/MarkdownEditor';
 import PublicPrivateToggle from '@components/userPage/PublicPrivateToggle';
 import Button from '@components/common/Button';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+import NoteTitleInput from '@components/userPage/NoteTitleInput';
 
 // decodeURIComponent(params.userNickname as string)
 export default function EditNote() {
   const theme = 'light';
   const params = useParams();
   const [isPublic, setIsPublic] = useState(false);
+  const [noteTitle, setNoteTitle] = useState('글 제목');
+  const inputRef = useRef(null);
   return (
     <div>
-      <div className="flex flex-row justify-between mb-3">
-        <div className="flex flex-row gap-3">
-          <Text type="h1">글제목</Text>
+      <div className="flex flex-row justify-between items-center mb-3">
+        <NoteTitleInput
+          theme={theme}
+          noteTitle={noteTitle}
+          onChange={(e) => setNoteTitle(e.target.value)}
+        />
+        <div className="flex flex-row items-center gap-3">
           <PublicPrivateToggle
             onClick={() => setIsPublic(!isPublic)}
             scope={isPublic ? 'Public' : 'Private'}
             theme={theme}
           />
+          <Button label={isPublic ? '게시하기' : '저장하기'} color="charcol" />
         </div>
-        <Button label="저장하기" color="charcol" />
       </div>
       <MarkdownEditor theme="light" />
     </div>
