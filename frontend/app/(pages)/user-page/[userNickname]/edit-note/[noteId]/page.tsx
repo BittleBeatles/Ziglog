7ef4c01904f57ee2d32c3edc8dbccceff78eead7
hanulkimm <hanulkim2000@naver.com>
2023-10-29 22:06:33 +0900

    design: 전체공개/비공개 버튼 모드 추가
    - feat:  onCLick function 도 추가

--- frontend/app/(pages)/user-page/[userNickname]/edit-note/[noteId]/page.tsx
+++ frontend/app/(pages)/user-page/[userNickname]/edit-note/[noteId]/page.tsx
@@ -4,20 +4,27 @@ import Text from '@components/common/Text';
 import MarkdownEditor from '@components/userPage/MarkdownEditor';
 import PublicPrivateToggle from '@components/userPage/PublicPrivateToggle';
 import Button from '@components/common/Button';
+import { useState } from 'react';
+
 // decodeURIComponent(params.userNickname as string)
 export default function EditNote() {
+  const theme = 'light';
   const params = useParams();
-
+  const [isPublic, setIsPublic] = useState(false);
   return (
     <div>
-      <div className="flex flex-row justify-between">
+      <div className="flex flex-row justify-between mb-3">
         <div className="flex flex-row gap-3">
           <Text type="h1">글제목</Text>
-          <PublicPrivateToggle scope="Private" />
+          <PublicPrivateToggle
+            onClick={() => setIsPublic(!isPublic)}
+            scope={isPublic ? 'Public' : 'Private'}
+            theme={theme}
+          />
         </div>
-        <Button label="저장" color="charcol" />
+        <Button label="저장하기" color="charcol" />
       </div>
-      <MarkdownEditor />
+      <MarkdownEditor theme="light" />
     </div>
   );
 }
