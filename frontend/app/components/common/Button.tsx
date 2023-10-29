781abaf234bbbc7b080377e0f371edccaf768dec
hanulkimm <hanulkim2000@naver.com>
2023-10-29 22:58:35 +0900

    fix: 불필요한 div 제거

--- frontend/app/components/common/Button.tsx
+++ frontend/app/components/common/Button.tsx
@@ -13,12 +13,10 @@ const COLOR_VARIANTS = {
 
 export default function Button({ label, color, ...rest }: ButtonProps) {
   return (
-    <div>
-      <button
-        className={`${COLOR_VARIANTS[color]} font-bold  colorClass text-white w-fit h-fit px-3 py-2 rounded-full`}
-      >
-        {label}
-      </button>
-    </div>
+    <button
+      className={`${COLOR_VARIANTS[color]} font-bold  colorClass text-white w-fit h-fit px-3 py-2 rounded-full`}
+    >
+      {label}
+    </button>
   );
 }
