94c8e78d213db70289107695dfe7de97281ba79c
hanulkimm <hanulkim2000@naver.com>
2023-10-29 23:28:01 +0900

    feat: 괄호 변환
    - 대괄호 2개 시, 북마크 모달을 어떻게 띄워야 할까

--- frontend/app/components/userPage/MarkdownEditor.tsx
+++ frontend/app/components/userPage/MarkdownEditor.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import dynamic from 'next/dynamic';
+import BookmarkList from './SideBar/BookmarkList';
 const MDEditor = dynamic(() => import('@uiw/react-md-editor'), {
   ssr: false,
 });
@@ -10,14 +11,25 @@ interface MarkdownEditorProps {
 
 export default function MarkdownEditor({ theme }: MarkdownEditorProps) {
   const [value, setValue] = useState<string>();
+  const renderValueWithBookmarkList = (text: string) => {
+    return text.replace(/\[\[/g, '[[bookmark-list]]');
+  };
+
+  const handleChange = (v: string) => {
+    const updatedValue = renderValueWithBookmarkList(v);
+    setValue(updatedValue);
+  };
   return (
     <div>
       <MDEditor
         data-color-mode={theme}
         height={600}
         value={value}
-        onChange={(v) => setValue(v || '')}
+        onChange={(v) => handleChange(v || '')}
       ></MDEditor>
+      {value && value.includes('[[bookmark-list]]') && (
+        <BookmarkList noteList={[]} theme={theme} />
+      )}
     </div>
   );
 }
