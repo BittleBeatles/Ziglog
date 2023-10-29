aed381de963830660a38de10ce3a3d3d4123ced5
박영서 <pj0642@gmail.com>
2023-10-29 21:14:51 +0900

    feat: 이 글을 참조하는 글 목록 Controller 작성

--- backend/src/main/java/com/ziglog/ziglog/domain/note/controller/NoteController.java
+++ backend/src/main/java/com/ziglog/ziglog/domain/note/controller/NoteController.java
@@ -1,13 +1,28 @@
 package com.ziglog.ziglog.domain.note.controller;
 
+import com.ziglog.ziglog.domain.note.dto.response.QuotationListResponseDto;
+import com.ziglog.ziglog.domain.note.service.NoteService;
+import com.ziglog.ziglog.global.util.dto.ResponseDto;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+@RequiredArgsConstructor
 @RequestMapping("/note")
 public class NoteController {
 
+    private final NoteService noteService;
+
+    @GetMapping("/ref")
+    public ResponseDto<QuotationListResponseDto> getNotesQuoting(@RequestParam("note_id") Long noteId) throws Exception{
+        return ResponseDto.of(200, "success",
+                QuotationListResponseDto.toDto(noteService.getNote(noteId)));
+    }
 
 }
