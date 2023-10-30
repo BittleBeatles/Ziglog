515c1ccdac5e3d358d5578b68cd23382c4a0eac1
Hanul Kim <hanulkim2000@korea.ac.kr>
2023-10-30 16:16:27 +0900

    feat: 노트 수정 api 요청 추가

--- frontend/app/api/note/note.ts
+++ frontend/app/api/note/note.ts
@@ -1,16 +1,32 @@
-import { publicFetch } from '..';
+import { privateFetch, publicFetch } from '..';
 import { API_URL } from '@api/constants';
-import { NoteInfo } from './types';
+import { NoteInfo, EditNoteInfo } from './types';
 import { ApiSuccessResponse } from '@api/types';
 
 export type NoteApiData = ApiSuccessResponse<NoteInfo>;
-
+export type EditNoteApiResponse = ApiSuccessResponse<null>;
 export function getNoteInfo(noteId: number): Promise<NoteInfo> {
   return publicFetch<NoteApiData>(`${API_URL}/note/${noteId}`, {
     method: 'GET',
   })
     .then((res) => {
       return Promise.resolve(res.body.data);
+    })
+    .catch((err) => {
+      throw err;
+    });
+}
+
+export function sendEditNoteInfoRequest(
+  noteId: number,
+  params: EditNoteInfo
+): Promise<string | void> {
+  return privateFetch<EditNoteApiResponse>(`${API_URL}/note/${noteId}`, {
+    method: 'PUT',
+    body: params,
+  })
+    .then((res) => {
+      return Promise.resolve('[note edit succeeded]');
     })
     .catch((err) => {
       throw err;
