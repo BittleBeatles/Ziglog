6e1da995dc040d5fcd6a3fd6e823b0e43e2fec84
박영서 <pj0642@gmail.com>
2023-10-30 15:59:52 +0900

    fix: Request & Response DTO들에 @Getter 애노테이션을 추가 - Swagger에서 잘 보임

--- backend/src/main/java/com/ziglog/ziglog/domain/note/dto/response/ReadNoteResponseDto.java
+++ backend/src/main/java/com/ziglog/ziglog/domain/note/dto/response/ReadNoteResponseDto.java
@@ -2,10 +2,13 @@ package com.ziglog.ziglog.domain.note.dto.response;
 
 import com.ziglog.ziglog.domain.note.entity.Note;
 import lombok.Builder;
+import lombok.Getter;
+
 import java.util.List;
 
 
 @Builder
+@Getter
 public class ReadNoteResponseDto {
     private Long noteId;
     private String title;
