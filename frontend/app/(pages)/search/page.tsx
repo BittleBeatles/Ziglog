8b2f21fe0ff3d3cd48451edd1bcc0ebc41aebf8e
ababwjdgusdk <ababwjdgusdk@naver.com>
2023-10-30 17:51:20 +0900

    feat: 글 결과 api 연결 진행 중

--- frontend/app/(pages)/search/page.tsx
+++ frontend/app/(pages)/search/page.tsx
@@ -1,41 +1,41 @@
+'use client';
+import { useEffect, useState } from 'react';
+import { API_URL } from '@api/constants';
 import GlobalSearchInput from '@components/search/GlobalSearchInput';
 import GlobalSearchResult from '@components/search/GlobalSearchResult';
 
+export interface Search {
+  noteId: number;
+  title: string;
+  content: string;
+  nickname: string;
+  isPublic: boolean;
+  bookmarkCount: number;
+  postTime: string;
+  editTime: string | null;
+}
+
 export default function Search() {
-  const searchData = {
-    statusCode: 200,
-    message: 'success',
-    data: [
-      {
-        noteId: 1,
-        title: 'hiyo',
-        content: 'Header',
-        nickname: 'hanul',
-        isPublic: true,
-        bookmarkCount: 100,
-        postTime: '2023-09-29 12:34',
-        editTime: null,
-      },
-      {
-        noteId: 5,
-        title: 'hiyooo',
-        content: '지그로그 많이 이용해주세요. 정말 재밌거든요.',
-        nickname: 'hyeona',
-        isPublic: true,
-        bookmarkCount: 80,
-        postTime: '2023-10-29 12:34',
-        editTime: '2023-10-29 22:34',
-      },
-    ],
-  };
+  const [searchData, setSearchData] = useState<Search[]>([]);
+  const [keyword, setKeyword] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = (
+        await fetch(`${API_URL}/api/search?keyword=${keyword}`)
+      ).then((res) => res.json());
+      setSearchData(data.data);
+    }
+    fetchData();
+  }, [keyword]);
 
   return (
     <div className="flex flex-col justify-cneter items-center">
       <h1>검색페이지입니다.</h1>
       <div className="w-2/3">
-        <GlobalSearchInput />
+        <GlobalSearchInput onChange={(e) => setKeyword(e.target.value)} />
         <div>
-          {searchData.data.map((result) => (
+          {searchData.map((result) => (
             <GlobalSearchResult
               key={result.noteId}
               noteId={result.noteId}
