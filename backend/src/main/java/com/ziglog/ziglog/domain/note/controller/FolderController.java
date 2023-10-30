9d4dac8716fddd0b7b27b4f672514fcd304769ba
박영서 <pj0642@gmail.com>
2023-10-30 13:18:25 +0900

    docs: MemberController, FolderController에 Swagger API 명세의 설명을 추가

--- backend/src/main/java/com/ziglog/ziglog/domain/note/controller/FolderController.java
+++ backend/src/main/java/com/ziglog/ziglog/domain/note/controller/FolderController.java
@@ -5,6 +5,8 @@ import com.ziglog.ziglog.domain.note.dto.request.ModifyFolderNameRequestDto;
 import com.ziglog.ziglog.domain.note.service.NoteService;
 import com.ziglog.ziglog.global.auth.entity.CustomUserDetails;
 import com.ziglog.ziglog.global.util.dto.ResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -14,23 +16,32 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/folder")
+@Tag(name="폴더 컨트롤러")
 public class FolderController {
 
     private final NoteService noteService;
 
+    @Operation(summary = "폴더 생성",
+            description = "폴더 이름과 부모 폴더를 바탕으로 새로운 폴더를 생성"
+    )
     @PostMapping("")
     public ResponseDto<Void> createFolder(@AuthenticationPrincipal CustomUserDetails userDetails, CreateFolderRequestDto createFolderRequestDto) throws Exception{
         noteService.createFolder(userDetails.member(), createFolderRequestDto.toEntity());
         return ResponseDto.of(200, "success");
     }
 
-
+    @Operation(summary = "폴더명 변경",
+            description = "해당 폴더의 이름을 변경"
+    )
     @PutMapping("")
     public ResponseDto<Void> modifyFolder(@AuthenticationPrincipal CustomUserDetails userDetails, ModifyFolderNameRequestDto modifyFolderNameRequestDto) throws Exception {
         noteService.modifyFolder(userDetails.member(), modifyFolderNameRequestDto.toEntity());
         return ResponseDto.of(200, "success");
     }
 
+    @Operation(summary = "폴더 삭제",
+            description = "해당 폴더를 삭제"
+    )
     @DeleteMapping("/{folderId}")
     public ResponseDto<Void> deleteFolder(@AuthenticationPrincipal CustomUserDetails userDetails, @PathVariable("folderId") Long folderId) throws Exception{
         noteService.deleteNote(userDetails.member(), folderId);
