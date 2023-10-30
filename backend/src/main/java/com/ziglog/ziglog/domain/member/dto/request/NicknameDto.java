6b5d8085b1f39860d3204ae080ebf340ba29873e
박영서 <pj0642@gmail.com>
2023-10-30 13:05:29 +0900

    feat: 닉네임 확인을 위한 NicknameDto 재생성

---
+++ backend/src/main/java/com/ziglog/ziglog/domain/member/dto/request/NicknameDto.java
@@ -0,0 +1,10 @@
+package com.ziglog.ziglog.domain.member.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class NicknameDto {
+    private String nickname;
+}
