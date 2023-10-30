8516661af2ca350ae6a4b9fb6a1c02580c3e319a
ababwjdgusdk <ababwjdgusdk@naver.com>
2023-10-30 13:14:18 +0900

    feat: SearchResult map함수로 list 나열

--- frontend/app/(pages)/search/page.tsx
+++ frontend/app/(pages)/search/page.tsx
@@ -2,22 +2,54 @@ import GlobalSearchInput from '@components/search/GlobalSearchInput';
 import GlobalSearchResult from '@components/search/GlobalSearchResult';
 
 export default function Search() {
+  const searchData = {
+    statusCode: 200,
+    message: 'success',
+    data: [
+      {
+        noteId: 1,
+        title: 'hiyo',
+        content: 'Header',
+        nickname: 'hanul',
+        isPublic: true,
+        bookmarkCount: 100,
+        postTime: '2023-09-29 12:34',
+        editTime: null,
+      },
+      {
+        noteId: 5,
+        title: 'hiyooo',
+        content: '지그로그 많이 이용해주세요. 정말 재밌거든요.',
+        nickname: 'hyeona',
+        isPublic: true,
+        bookmarkCount: 80,
+        postTime: '2023-10-29 12:34',
+        editTime: '2023-10-29 22:34',
+      },
+    ],
+  };
+
   return (
     <div className="flex flex-col justify-cneter items-center">
       <h1>검색페이지입니다.</h1>
       <div className="w-2/3">
         <GlobalSearchInput />
-        <GlobalSearchResult
-          noteId={1}
-          title="안녕하세요"
-          preview="코드실행 결과리스트 테이블 뷰에서 항목을 선택하면 나오는 뷰를 디자인하였습니다. Table View (제약 조건을 주었습니다) Table View CellImage ViewLableButtonFile->New->Cocoa Touch Class -> Clas 이름 작성을 하여 코드실행 결과리스트 테이블 뷰에서 항목을 선택하면 나오는 뷰를 디자인하였습니다. Table View (제약 조건을 주었습니다) Table View CellImage ViewLableButtonFile->New->Cocoa Touch Class -> Clas 이름 작성을 하여"
-          nickname="markLee"
-          isPublic={true}
-          bookmarkCount={82}
-          postTime="2023-10-26 12:23"
-          editTime={null}
-          theme="light"
-        ></GlobalSearchResult>
+        <div>
+          {searchData.data.map((result) => (
+            <GlobalSearchResult
+              key={result.noteId}
+              noteId={result.noteId}
+              title={result.title}
+              preview={result.content}
+              nickname={result.nickname}
+              isPublic={result.isPublic}
+              bookmarkCount={result.bookmarkCount}
+              postTime={result.postTime}
+              editTime={result.editTime}
+              theme="light"
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
