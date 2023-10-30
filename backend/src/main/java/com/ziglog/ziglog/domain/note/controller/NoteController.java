c78ac306fb8225b5033fad388ffd682a9c4892ee
박영서 <pj0642@gmail.com>
2023-10-30 15:38:21 +0900

    feat : 폴더 탐색 로직 변경 - fix: 토큰 갱신 응답에서 사용할 DTO를 작성

--- backend/src/main/java/com/ziglog/ziglog/domain/note/controller/NoteController.java
+++ backend/src/main/java/com/ziglog/ziglog/domain/note/controller/NoteController.java
@@ -25,24 +25,21 @@ public class NoteController {
     private final NoteService noteService;
 
     @Operation(summary = "사용자의 문서를 조회",
-            description = "닉네임을 통해 해당 사용자의 개인 페이지 좌측 상단에서 볼 수 있는 폴더 + 문서를 조회"
-    )
+            description = "닉네임을 통해 해당 사용자의 개인 페이지 좌측 상단에서 볼 수 있는 폴더 + 문서를 조회")
     @GetMapping("")
-    public ResponseDto<RetrieveFolderResponseDto> listNoteOf(@RequestParam("nickname") String nickname) throws Exception {
-        return ResponseDto.of(RetrieveFolderResponseDto.toDto(noteService.getRootFolder(nickname)));
+    public ResponseDto<RetrieveFolderResponseDto> listNoteOf(@AuthenticationPrincipal CustomUserDetails userDetails, @RequestParam("nickname") String nickname) throws Exception {
+        return ResponseDto.of(RetrieveFolderResponseDto.toDto(noteService.getRootFolder(nickname), userDetails.member()));
     }
 
     @Operation(summary = "이 글을 인용하고 있는 글들의 목록을 불러 옴",
-            description = "이 글을 인용하고 있는 글의 저자 닉네임과 글의 제목 목록을 조회"
-    )
+            description = "이 글을 인용하고 있는 글의 저자 닉네임과 글의 제목 목록을 조회")
     @GetMapping("/ref")
     public ResponseDto<QuotationListResponseDto> getNotesQuoting(@RequestParam("noteId") Long noteId) throws Exception{
         return ResponseDto.of(QuotationListResponseDto.toDto(noteService.getNote(noteId)));
     }
 
     @Operation(summary = "노트 삭제",
-            description = "해당 아이디의 노트를 삭제"
-    )
+            description = "해당 아이디의 노트를 삭제")
     @DeleteMapping("")
     public ResponseDto<Void> deleteNote(@AuthenticationPrincipal CustomUserDetails userDetails, @RequestParam("noteId") Long noteId) throws Exception {
         noteService.deleteNote(userDetails.member(), noteId);
@@ -50,8 +47,7 @@ public class NoteController {
     }
 
     @Operation(summary = "빈 새 노트를 생성",
-            description = "비어있는 새 노트를 생성"
-    )
+            description = "비어있는 새 노트를 생성")
     @PostMapping("")
     public ResponseDto<Void> createNote(@AuthenticationPrincipal CustomUserDetails userDetails, @RequestBody CreateNoteRequestDto createNoteRequestDto) throws Exception{
         noteService.createNote(userDetails.member(), createNoteRequestDto.getFolderId());
@@ -59,8 +55,7 @@ public class NoteController {
     }
 
     @Operation(summary = "해당 노트의 내용을 수정",
-            description = "해당 노트의 내용(제목, 내용, 이 글이 인용하고 있는 글 목록)을 수정"
-    )
+            description = "해당 노트의 내용(제목, 내용, 이 글이 인용하고 있는 글 목록)을 수정")
     @PutMapping("/{noteId}")
     public ResponseDto<Void> modifyNote(@AuthenticationPrincipal CustomUserDetails userDetails, @PathVariable("noteId") Long noteId, @RequestBody ModifyNoteRequestDto modifyNoteRequestDto) throws Exception {
         noteService.modifyNote(userDetails.member(), modifyNoteRequestDto.toEntity(userDetails.member(), noteId));
@@ -68,8 +63,7 @@ public class NoteController {
     }
 
     @Operation(summary = "노트 읽기",
-            description = "해당 Id의 노트의 상세 정보를 읽어 옴"
-    )
+            description = "해당 Id의 노트의 상세 정보를 읽어 옴")
     @GetMapping("/{noteId}")
     public ResponseDto<ReadNoteResponseDto> readNote(@AuthenticationPrincipal CustomUserDetails userDetails, @PathVariable("noteId") Long noteId) throws Exception {
         //TODO 이 사용자가 해당 노트를 읽을 수 있는지 없는지 확인하는 로직이 필요
--- backend/src/main/java/com/ziglog/ziglog/domain/note/dto/response/RetrieveFolderResponseDto.java
+++ backend/src/main/java/com/ziglog/ziglog/domain/note/dto/response/RetrieveFolderResponseDto.java
@@ -1,5 +1,6 @@
 package com.ziglog.ziglog.domain.note.dto.response;
 
+import com.ziglog.ziglog.domain.member.entity.Member;
 import com.ziglog.ziglog.domain.note.entity.Folder;
 import com.ziglog.ziglog.domain.note.entity.Note;
 import lombok.Builder;
@@ -35,22 +36,26 @@ public class RetrieveFolderResponseDto {
         private Boolean isPublic;
     }
 
-    public static RetrieveFolderResponseDto toDto(Folder folder){
+    public static RetrieveFolderResponseDto toDto(Folder folder, Member member){
         return new RetrieveFolderResponseDto(
                     FolderDto.builder()
                             .type("folder")
                             .folderId(folder.getId())
                             .title(folder.getTitle())
-                            .notes(folder.getNotes().stream().sorted(Comparator.comparing(Note::getTitle)).map(note ->
-                                NoteDto.builder()
-                                        .type("note")
-                                        .noteId(note.getId())
-                                        .title(note.getTitle())
-                                        .isPublic(note.isPublic())
-                                        .build()).toList()//이름으로 오름차순 정렬
+                            .notes(folder.getNotes().stream().sorted(Comparator.comparing(Note::getTitle))
+                                            .filter(note -> note.isPublic() || note.getAuthor().equals(member))
+                                            .map(note ->
+                                                NoteDto.builder()
+                                                        .type("note")
+                                                        .noteId(note.getId())
+                                                        .title(note.getTitle())
+                                                        .isPublic(note.isPublic())
+                                                        .build()
+                                            ).toList()//이름으로 오름차순 정렬
                             )
                             .folderList(folder.getChildren().stream().sorted(Comparator.comparing(Folder::getTitle))
-                            .map(RetrieveFolderResponseDto::toDto).toList())//이름으로 오름차순 정렬해서 주기
+                                    .map((Folder folder1) -> toDto(folder1, member))
+                                    .toList())//이름으로 오름차순 정렬해서 주기
                             .build()
                 );
     }
