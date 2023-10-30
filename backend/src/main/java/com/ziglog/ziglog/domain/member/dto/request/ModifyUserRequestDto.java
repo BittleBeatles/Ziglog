1011d92e1cddc6e233b13a92da9c918fb492702a
박영서 <pj0642@gmail.com>
2023-10-30 13:03:47 +0900

    feat: 사용자 닉네임 변경과 프로필 변경 요청을 한 번에 하기로 변경함

--- backend/src/main/java/com/ziglog/ziglog/domain/member/dto/request/ModifyUserRequestDto.java
+++ backend/src/main/java/com/ziglog/ziglog/domain/member/dto/request/ModifyUserRequestDto.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
-public class NicknameDto {
+public class ModifyUserRequestDto {
     private String nickname;
-
+    private String profileUrl;
 }
