b3c9d0a6200d55ac3d59b7e6e99a1043a6c9eaf7
emmajane <emmajane@naver.com>
2023-10-30 14:21:51 +0900

    fix: quotation modal 수정

--- frontend/app/components/userPage/QuotationModal.tsx
+++ frontend/app/components/userPage/QuotationModal.tsx
@@ -9,28 +9,41 @@ export default function QuotationModal({
   theme = 'light',
   ...rest
 }: QuotationModalProps) {
+  const bookmarks = [
+    {
+      noteId: 1,
+      title: '메롱입니다',
+      nickname: 'frog_slayer',
+      isBookmarked: true,
+    },
+    {
+      noteId: 2,
+      title: '안녕하세요',
+      nickname: 'hanulkim',
+      isBookmarked: true,
+    },
+    {
+      noteId: 3,
+      title: 'Javascript',
+      nickname: 'yonseong2',
+      isBookmarked: false,
+    },
+  ];
   return (
-    <div
-      className={`${THEME_VARIANTS[theme]} w-80 h-fit rounded-md border p-2`}
-    >
-      <SingleQuotation
-        theme={theme}
-        isBookMarked={true}
-        title="메롱입니다"
-        nickname="frog_slayer"
-      />
-      <SingleQuotation
-        theme={theme}
-        isBookMarked={true}
-        title="안녕하세요"
-        nickname="hanulkim"
-      />
-      <SingleQuotation
-        theme={theme}
-        isBookMarked={false}
-        title="Javascript"
-        nickname="yonseong2"
-      />
+    <div>
+      <div
+        className={`${THEME_VARIANTS[theme]} w-80 h-fit rounded-md border p-2`}
+      >
+        {bookmarks.map((bookmark, index) => (
+          <SingleQuotation
+            key={index}
+            theme={theme}
+            isBookMarked={bookmark.isBookmarked}
+            title={bookmark.title}
+            nickname={bookmark.nickname}
+          ></SingleQuotation>
+        ))}
+      </div>
     </div>
   );
 }
