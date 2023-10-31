b808508a88a8785b40f1a8bcdcebeae8676afb7a
Hanul Kim <hanulkim2000@korea.ac.kr>
2023-10-31 09:29:57 +0900

    fix: 버튼에 ...rest 추가

--- frontend/app/components/common/Button.tsx
+++ frontend/app/components/common/Button.tsx
@@ -14,6 +14,7 @@ const COLOR_VARIANTS = {
 export default function Button({ label, color, ...rest }: ButtonProps) {
   return (
     <button
+      {...rest}
       className={`${COLOR_VARIANTS[color]} font-bold  colorClass text-white w-fit h-fit px-3 py-2 rounded-full`}
     >
       {label}
