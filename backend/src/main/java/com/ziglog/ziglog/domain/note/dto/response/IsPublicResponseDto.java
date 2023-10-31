6e0b0ba626b4ca5b07ede8583e55713cdd53f848
박영서 <pj0642@gmail.com>
2023-10-31 10:04:47 +0900

    feat: 노트 공개 여부 변경 API 작성 - 컨트롤러 및 DTO를 생성

---
+++ backend/src/main/java/com/ziglog/ziglog/domain/note/dto/response/IsPublicResponseDto.java
@@ -0,0 +1,17 @@
+package com.ziglog.ziglog.domain.note.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+public class IsPublicResponseDto {
+    private Boolean isPublic;
+
+    private IsPublicResponseDto(Boolean isPublic){
+        this.isPublic = isPublic;
+    }
+
+    public static IsPublicResponseDto toDto(Boolean isPublic){
+        return new IsPublicResponseDto(isPublic);
+    }
+}
