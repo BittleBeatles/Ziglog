c5e38686357c33067f203f81594e1477d4440942
ababwjdgusdk <ababwjdgusdk@naver.com>
2023-10-31 17:41:07 +0900

    feat: 글 검색 결과 무한 스크롤 구현

--- frontend/app/(pages)/search/page.tsx
+++ frontend/app/(pages)/search/page.tsx
@@ -1,53 +1,152 @@
 'use client';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import GlobalSearchInput from '@components/search/GlobalSearchInput';
 import GlobalSearchResult from '@components/search/GlobalSearchResult';
-import { SearchInfo } from '@api/search/types';
+import useDebounce from '@src/hooks/useDebounce';
+import { SearchResult } from '@api/search/types';
 import { getSearchInfo } from '@api/search/search';
+import Text from '@components/common/Text';
 
 export default function Search() {
-  const [keyword, setKeyword] = useState<string | null>(null);
-  const [searchData, setSearchData] = useState<SearchInfo | null>(null);
+  const [keyword, setKeyword] = useState<string>('');
+  const [searchData, setSearchData] = useState<{
+    data: { notes: SearchResult[] };
+  } | null>(null);
+  const [page, setPage] = useState(1); // 페이지 번호
+  const [loading, setLoading] = useState(false); // 데이터 로드 중인지 여부
+  const [hasMore, setHasMore] = useState(true); // 더 많은 페이지가 있는지 여부
+  const containerRef = useRef<HTMLDivElement | null>(null); // 스크롤 컨테이너 ref
+  const perPage = 10;
 
-  useEffect(() => {
-    async function fetchData() {
-      setSearchData(null);
-      if (keyword) {
-        try {
-          const data = await getSearchInfo(keyword);
-          setSearchData(data || null);
-        } catch (error) {
-          console.error('Error fetching data:', error);
-          setSearchData(null); // Set an empty array on error
-        }
-      } else {
-        setSearchData(null); // Set an empty array when keyword is empty
+  const debouncedKeyword = useDebounce(keyword, 500);
+
+  // 스크롤 이벤트 핸들러
+  const handleScroll = () => {
+    if (
+      containerRef.current &&
+      containerRef.current.scrollHeight - containerRef.current.scrollTop ===
+        containerRef.current.clientHeight
+    ) {
+      if (!loading && hasMore) {
+        setLoading(true);
+        setPage(page + 1);
       }
     }
-    fetchData();
-  }, [keyword]);
+  };
+
+  useEffect(() => {
+    async function fetchMoreData(debouncedKeyword: string) {
+      try {
+        const response = await getSearchInfo(debouncedKeyword, page, perPage);
+        const newData = response.data;
+
+        if (newData && newData.notes.length > 0) {
+          setSearchData((prevData) => ({
+            data: {
+              notes: [...(prevData?.data?.notes || []), ...newData.notes],
+            },
+          }));
+        } else {
+          setHasMore(false);
+        }
+      } catch (error) {
+        console.error('Error fetching more data:', error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    if (!debouncedKeyword) {
+      return;
+    }
+
+    if (page === 1) {
+      setSearchData({ data: { notes: [] } });
+    }
+
+    if (debouncedKeyword) {
+      fetchMoreData(debouncedKeyword);
+    }
+  }, [debouncedKeyword, page]);
+
+  // // fetchMoreData 함수를 블록 외부에서 선언
+  // async function fetchMoreData(debouncedKeyword: string) {
+  //   try {
+  //     const response = await getSearchInfo(debouncedKeyword, page, perPage);
+  //     const newData = response.data;
+
+  //     if (newData && newData.notes.length > 0) {
+  //       setSearchData((prevData) => ({
+  //         data: {
+  //           notes: [...(prevData?.data?.notes || []), ...newData.notes],
+  //         },
+  //       }));
+  //     } else {
+  //       setHasMore(false);
+  //     }
+  //   } catch (error) {
+  //     console.error('Error fetching more data:', error);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // }
+
+  // // 스크롤 이벤트 핸들러
+  // const handleScroll = () => {
+  //   if (
+  //     containerRef.current &&
+  //     containerRef.current.scrollHeight - containerRef.current.scrollTop ===
+  //       containerRef.current.clientHeight
+  //   ) {
+  //     if (!loading && hasMore) {
+  //       setLoading(true);
+  //       setPage(page + 1);
+  //     }
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   // 입력 없을 시.
+  //   if (!debouncedKeyword) {
+  //     return;
+  //   }
+
+  //   if (page === 1) {
+  //     setSearchData({ data: { notes: [] } });
+  //   }
+
+  //   if (debouncedKeyword) {
+  //     // 함수 호출로 변경
+  //     fetchMoreData(debouncedKeyword);
+  //   }
+  // }, [debouncedKeyword, page, fetchMoreData]);
 
   return (
     <div className="flex flex-col justify-cneter items-center">
       <h1>검색페이지입니다.</h1>
       <div className="w-2/3">
         <GlobalSearchInput onChange={(e) => setKeyword(e.target.value)} />
-        <div>
-          {searchData &&
-            searchData.data.notes.map((result) => (
-              <GlobalSearchResult
-                key={result.noteId}
-                noteId={result.noteId}
-                title={result.title}
-                preview={result.preview}
-                nickname={result.nickname}
-                isPublic={result.isPublic}
-                bookmarkCount={result.bookmarkCount}
-                postTime={result.postTime}
-                editTime={result.editTime}
-                theme="light"
-              />
-            ))}
+        <div className="h-full " onScroll={handleScroll} ref={containerRef}>
+          {searchData && searchData.data.notes.length > 0
+            ? searchData.data.notes.map((result) => (
+                <GlobalSearchResult
+                  key={result.noteId}
+                  noteId={result.noteId}
+                  title={result.title}
+                  preview={result.preview}
+                  nickname={result.nickname}
+                  isPublic={result.isPublic}
+                  bookmarkCount={result.bookmarkCount}
+                  postTime={result.postTime}
+                  editTime={result.editTime}
+                  theme="light"
+                />
+              ))
+            : debouncedKeyword && (
+                <div>
+                  <Text type="p">{'검색 결과가 없습니다.'}</Text>
+                </div>
+              )}
         </div>
       </div>
     </div>
--- frontend/app/api/search/search.ts
+++ frontend/app/api/search/search.ts
@@ -5,9 +5,17 @@ import { ApiSuccessResponse } from '@api/types';
 
 export type SearchApiData = ApiSuccessResponse<SearchInfo>;
 
-export function getSearchInfo(debouncedKeyword: string): Promise<SearchInfo> {
+export function getSearchInfo(
+  debouncedKeyword: string,
+  page: number,
+  perPage: number
+): Promise<SearchInfo> {
   return publicFetch<SearchApiData>(
+<<<<<<< HEAD
     `${API_URL}/search?keyword=${debouncedKeyword}`,
+=======
+    `${API_URL}/search?keyword=${debouncedKeyword}&page=${page}&perPage=${perPage}`,
+>>>>>>> 8ecca5a (feat: 글 검색 결과 무한 스크롤 구현)
     {
       method: 'GET',
     }
