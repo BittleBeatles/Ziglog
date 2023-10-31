488489697bf33c271e17ed6783132ff0c3a3285c
박영서 <pj0642@gmail.com>
2023-10-31 10:41:31 +0900

    feat: 검색 요청 Dt 작성

---
+++ backend/src/main/java/com/ziglog/ziglog/domain/note/dto/request/SearchRequestDto.java
@@ -0,0 +1,10 @@
+package com.ziglog.ziglog.domain.note.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class SearchRequestDto {
+    private String keyword;
+}
