f7343af8e6ea20ef926fb63baf9f33de86ba766b
Hanul Kim <hanulkim2000@korea.ac.kr>
2023-10-31 14:45:20 +0900

    feat: 참조한 리스트 불러오는 api 추가

--- frontend/app/api/note/note.ts
+++ frontend/app/api/note/note.ts
@@ -1,12 +1,11 @@
 import { privateFetch, publicFetch } from '..';
 import { API_URL } from '@api/constants';
-import { NoteInfo, EditNoteParams, NotePublicStatus } from './types';
+import { NoteInfo, NoteRefListInfo } from './types';
 import { ApiSuccessResponse } from '@api/types';
 
 export type NoteApiData = ApiSuccessResponse<NoteInfo>;
-export type EditNoteApiResponse = ApiSuccessResponse<null>;
-export type PublicStatusApiResponse = ApiSuccessResponse<NotePublicStatus>;
 export type CreateNoteApiResponse = ApiSuccessResponse<null>;
+export type QuotationListApiResponse = ApiSuccessResponse<NoteRefListInfo>;
 
 export async function getNoteInfo(noteId: number): Promise<NoteInfo> {
   try {
@@ -19,42 +18,6 @@ export async function getNoteInfo(noteId: number): Promise<NoteInfo> {
   }
 }
 
-export async function sendEditNoteInfoRequest(
-  noteId: number,
-  body: EditNoteParams
-): Promise<string | void> {
-  try {
-    const res = await privateFetch<EditNoteApiResponse>(
-      `${API_URL}/note/${noteId}`,
-      {
-        method: 'PUT',
-        body: body,
-      }
-    );
-    return await Promise.resolve('[note edit succeeded]');
-  } catch (err) {
-    throw err;
-  }
-}
-
-export async function changeNotePublicStatusRequest(
-  noteId: number,
-  body: NotePublicStatus
-): Promise<NotePublicStatus> {
-  try {
-    const res = await privateFetch<PublicStatusApiResponse>(
-      `${API_URL}/note/${noteId}/public`,
-      {
-        method: 'PUT',
-        body: body,
-      }
-    );
-    return await Promise.resolve(res.body.data);
-  } catch (error) {
-    throw error;
-  }
-}
-
 export async function createNote(folderId: number, title: string) {
   return privateFetch<CreateNoteApiResponse>(`${API_URL}/note`, {
     method: 'POST',
@@ -62,6 +25,19 @@ export async function createNote(folderId: number, title: string) {
   })
     .then((res) => {
       return console.log(`${folderId}에 노트가 생성되었습니다.`);
+    })
+    .catch((err) => {
+      throw err;
+    });
+}
+
+export async function getReferenceList(noteId: number) {
+  return publicFetch<QuotationListApiResponse>(`${API_URL}/note/ref`, {
+    method: 'GET',
+    body: { noteId },
+  })
+    .then((res) => {
+      return res.body.data;
     })
     .catch((err) => {
       throw err;
