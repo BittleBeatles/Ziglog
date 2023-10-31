0322830e0650ebfec0fe29054bd5dd1cf289c939
emmajane <emmajane@naver.com>
2023-10-31 13:30:37 +0900

    fix: Button component 수정

--- frontend/app/components/common/Button.tsx
+++ frontend/app/components/common/Button.tsx
@@ -1,9 +1,10 @@
-'use client';
+// 'use client';
 import { ButtonHTMLAttributes } from 'react';
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   label: string;
   color: 'charcol' | 'red' | 'blue';
-  size: string;
+  onClick?: () => void;
+  size?: string;
 }
 
 const COLOR_VARIANTS = {
@@ -12,9 +13,16 @@ const COLOR_VARIANTS = {
   blue: 'bg-main-100',
 };
 
-export default function Button({ label, color, size, ...rest }: ButtonProps) {
+export default function Button({
+  label,
+  color,
+  onClick,
+  size = '',
+  ...rest
+}: ButtonProps) {
   return (
     <button
+      onClick={onClick}
       className={`${COLOR_VARIANTS[color]} ${size} font-bold colorClass text-white w-fit h-fit px-3 py-2 rounded-full`}
     >
       {label}
