1a96e6b99770c1750d988714700d35f60690a8e1
박영서 <pj0642@gmail.com>
2023-10-31 16:36:58 +0900

    feat: 현재 조회 중인 글의 북마크 여부 확인 컨트롤러 추가

---
+++ backend/src/main/java/com/ziglog/ziglog/domain/bookmark/dto/response/IsBookmarkedDto.java
@@ -0,0 +1,10 @@
+package com.ziglog.ziglog.domain.bookmark.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class IsBookmarkedDto {
+    private boolean isBookmarked;
+}
