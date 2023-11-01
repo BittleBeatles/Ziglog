62d77b409132f5ce96d9ff60ba290e4e88122eb9
emmajane <emmajane@naver.com>
2023-11-01 13:51:49 +0900

    feat: read note page API 연결

--- frontend/app/(pages)/user-page/[userNickname]/layout.tsx
+++ frontend/app/(pages)/user-page/[userNickname]/layout.tsx
@@ -2,18 +2,18 @@
 import Box from '@components/common/Box';
 import IconButton from '@components/common/IconButton';
 import SideBar from '@components/userPage/SideBar';
+import { useAppSelector } from '@store/store';
 import { ReactNode, useState } from 'react';
 
 export default function UserLayout({ children }: { children: ReactNode }) {
   const [isSideBarOpen, setSideBarOpen] = useState(true);
-  const theme = 'light';
-
+  const { theme, isLogin } = useAppSelector((state) => state.user);
   const sideBarToggle = () => {
     setSideBarOpen(false);
   };
 
   return (
-    <div className="flex bg-light-background-layout">
+    <div className={`${THEME_VARIANTS[theme]} flex`}>
       {isSideBarOpen && (
         <div className="w-1/6">
           <SideBar sideBarToggle={sideBarToggle} theme={theme} />
@@ -32,3 +32,8 @@ export default function UserLayout({ children }: { children: ReactNode }) {
     </div>
   );
 }
+
+const THEME_VARIANTS = {
+  light: 'bg-light-background-layout',
+  dark: 'bg-dark-background-layout text-white',
+};
