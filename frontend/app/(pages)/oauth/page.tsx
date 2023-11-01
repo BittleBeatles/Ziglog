eb9c78ce9762f1ddab0c759b93e1cbe1b9aa1fbc
Hanul Kim <hanulkim2000@korea.ac.kr>
2023-11-01 17:48:48 +0900

    fix:Oauth page에서 setMyInfo로 수정

--- frontend/app/(pages)/oauth/page.tsx
+++ frontend/app/(pages)/oauth/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useAppDispatch } from '@store/store';
 import { useEffect } from 'react';
-import { setUserInfo, setUserToken } from '@store/modules/userSlice';
+import { setMyInfo, setUserToken } from '@store/modules/userSlice';
 import { getMyInfo } from '@api/user/user';
 export default function OauthPage() {
   const dispatch = useAppDispatch();
@@ -14,7 +14,7 @@ export default function OauthPage() {
       // [USERINFO] 가져오기
       const result = await getMyInfo();
       if (result) {
-        dispatch(setUserInfo(result));
+        dispatch(setMyInfo(result));
         window.location.replace(`/user-page/${result.nickname}`);
       } else {
         window.location.replace('/');
