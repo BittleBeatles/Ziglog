b45d0e7946a3b86430ba21e76149edab61804e97
박영서 <pj0642@gmail.com>
2023-11-01 17:09:22 +0900

    fix: 토큰 재발급 로직 수정

--- backend/src/main/java/com/ziglog/ziglog/global/auth/controller/AuthController.java
+++ backend/src/main/java/com/ziglog/ziglog/global/auth/controller/AuthController.java
@@ -21,15 +21,16 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "인증 관련 요청 처리 컨트롤러")
 public class AuthController {
 
-    @Value("{jwt.access.header}")
+    @Value("${jwt.access.header}")
     private String accessTokenHeader;
 
     @GetMapping("/refresh")
     @Operation(summary = "토큰 갱신 요청",
             description = "쿠키의 리프레시 토큰으로 새 액세스 토큰과 새 리프레시 토큰을 발급")
-
     public ResponseDto<RefreshResponseDto> refresh(HttpServletResponse response) throws Exception {
         String accessToken = response.getHeader(accessTokenHeader);
+        log.info("reIssued access Token : {}",  accessToken);
+
         return ResponseDto.of(new RefreshResponseDto(accessToken));
     }
 }
