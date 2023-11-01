0e33f9674afc252233d1c9062a132493dfd7971d
박영서 <pj0642@gmail.com>
2023-11-01 13:51:58 +0900

    fix: 단일 필드인 AddBookmarkRequestDto에 기본 생성자를 추가

--- backend/src/main/java/com/ziglog/ziglog/domain/bookmark/dto/request/AddBookmarkRequestDto.java
+++ backend/src/main/java/com/ziglog/ziglog/domain/bookmark/dto/request/AddBookmarkRequestDto.java
@@ -2,8 +2,10 @@ package com.ziglog.ziglog.domain.bookmark.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class AddBookmarkRequestDto {
     private Long noteId;
