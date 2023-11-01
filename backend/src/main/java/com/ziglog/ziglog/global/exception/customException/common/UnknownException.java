2f11015e52ec1ea36c572f9b83f9f56fa3815729
박영서 <pj0642@gmail.com>
2023-11-01 22:11:20 +0900

    feat: CommonExceptionHandler 구현 - ExceptionCode의 이름을 CommonExceptionCode로 변경

--- backend/src/main/java/com/ziglog/ziglog/global/exception/customException/common/UnknownException.java
+++ backend/src/main/java/com/ziglog/ziglog/global/exception/customException/common/UnknownException.java
@@ -1,10 +1,10 @@
 package com.ziglog.ziglog.global.exception.customException.common;
 
-import com.ziglog.ziglog.global.exception.exceptionCode.ExceptionCode;
+import com.ziglog.ziglog.global.exception.exceptionCode.CommonExceptionCode;
 
 public class UnknownException extends RuntimeException {
     public UnknownException(){
-        super(ExceptionCode.UNKNOWN_EXCEPTION.getErrorMessage());
+        super(CommonExceptionCode.UNKNOWN_EXCEPTION.getErrorMessage());
     }
     public UnknownException(String message){
         super(message);
--- backend/src/main/java/com/ziglog/ziglog/global/exception/exceptionCode/CommonExceptionCode.java
+++ backend/src/main/java/com/ziglog/ziglog/global/exception/exceptionCode/CommonExceptionCode.java
@@ -3,7 +3,7 @@ package com.ziglog.ziglog.global.exception.exceptionCode;
 import lombok.Getter;
 
 @Getter
-public enum ExceptionCode {
+public enum CommonExceptionCode {
     UNKNOWN_EXCEPTION(500, "알 수 없는 예외가 발생했습니다."),
     METHOD_NOT_FOUND_EXCEPTION(405, "유효하지 않은 메서드입니다"),
     BAD_REQUEST_EXCEPTION(400, "잘못된 요청입니다");
@@ -11,7 +11,7 @@ public enum ExceptionCode {
     private final Integer errorCode;
     private final String errorMessage;
 
-    ExceptionCode(Integer errorCode, String errorMessage){
+    CommonExceptionCode(Integer errorCode, String errorMessage){
         this.errorCode = errorCode;
         this.errorMessage = errorMessage;
     }
