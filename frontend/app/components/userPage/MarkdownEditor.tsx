2229896bddc099493a9f111e6c1b3a205ae8d928
Hanul Kim <hanulkim2000@korea.ac.kr>
2023-11-02 11:07:16 +0900

    fix: temp

--- frontend/app/components/userPage/MarkdownEditor.tsx
+++ frontend/app/components/userPage/MarkdownEditor.tsx
@@ -1,45 +1,34 @@
 'use client';
-import { forwardRef, useEffect, useRef, useState } from 'react';
+import { InputHTMLAttributes, forwardRef, useState } from 'react';
 import dynamic from 'next/dynamic';
 
 const MDEditor = dynamic(() => import('@uiw/react-md-editor'), {
   ssr: false,
 });
 
-interface MarkdownEditorProps {
+interface MarkdownEditorProps extends InputHTMLAttributes<HTMLInputElement> {
   theme: 'dark' | 'light';
   preview: 'edit' | 'live' | 'preview';
   hideToolbar: boolean;
   height: number;
+  content: string;
 }
 
 const MarkdownEditor = forwardRef(
-  ({ theme, preview, hideToolbar, height }: MarkdownEditorProps, ref) => {
-    const [value, setValue] = useState<string>();
+  (
+    { content, theme, preview, hideToolbar, height }: MarkdownEditorProps,
+    ref
+  ) => {
+    // const [value, setValue] = useState<string>(content);
     const editorRef = ref;
     const renderValueWithBookmarkList = (text: string) => {
       return text.replace(/\[\[\]\]/g, '[[bookmark-list]]');
     };
 
-    const handleChange = (v: string) => {
-      const updatedValue = renderValueWithBookmarkList(v);
-      setValue(updatedValue);
-    };
-
-    // useEffect(() => {
-    //   if (editorRef.current && quotationModalRef.current) {
-    //     const editor = editorRef.current;
-    //     const quotationModal = quotationModalRef.current;
-    //     if (value && value.includes('[[bookmark-list]]')) {
-    //       console.log('value check');
-    //       const bookMarkListPosition = value?.indexOf('[[bookmark-list]]');
-    //       const editorRect = editor.getBoundingClientRect();
-    //       const top = editorRect.top + bookMarkListPosition;
-    //       quotationModal.style.top = top + 'px';
-    //       quotationModal.style.position = 'absolute';
-    //     }
-    //   }
-    // }, [value]);
+    // const handleChange = (v: string) => {
+    //   const updatedValue = renderValueWithBookmarkList(v);
+    //   setValue(updatedValue);
+    // };
 
     return (
       <div>
@@ -48,8 +37,9 @@ const MarkdownEditor = forwardRef(
           className="relative"
           data-color-mode={theme}
           height={height}
-          value={value}
-          onChange={(v) => handleChange(v || '')}
+          // defaultValue={content}
+          value={content}
+          // onChange={(v) => handleChange(v || '')}
           preview={preview}
           hideToolbar={hideToolbar}
         ></MDEditor>
