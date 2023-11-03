4182f407e34354da1cf63393053cf73307d92ab9
박영서 <pj0642@gmail.com>
2023-11-03 10:55:19 +0900

    feat: 글 읽기 응답에 게시 및 수정 시간 필드를 추가

--- backend/src/main/java/com/ziglog/ziglog/domain/note/dto/response/ReadNoteResponseDto.java
+++ backend/src/main/java/com/ziglog/ziglog/domain/note/dto/response/ReadNoteResponseDto.java
@@ -1,9 +1,11 @@
 package com.ziglog.ziglog.domain.note.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.ziglog.ziglog.domain.note.entity.Note;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 
@@ -17,6 +19,12 @@ public class ReadNoteResponseDto {
     private Boolean isPublic;
     private Integer bookmarkCount;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
+    private LocalDateTime postTime;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
+    private LocalDateTime editTime;
+
     public static ReadNoteResponseDto toDto(Note note){
         return ReadNoteResponseDto.builder()
                 .noteId(note.getId())
@@ -25,6 +33,8 @@ public class ReadNoteResponseDto {
                 .nickname(note.getAuthor().getNickname())
                 .isPublic(note.isPublic())
                 .bookmarkCount(note.getBookmarks().size())
+                .postTime(note.getPostDatetime())
+                .editTime(note.getEditDatetime())
                 .build();
     }
 }
