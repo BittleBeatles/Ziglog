dc28f5b4730735d278f3ef21d5d8128372c317c5
박영서 <pj0642@gmail.com>
2023-11-06 12:40:33 +0900

    refactor: MemberService 인터페이스에 Exception들을 구체적으로 명시

--- backend/src/main/java/com/ziglog/ziglog/domain/member/service/MemberService.java
+++ backend/src/main/java/com/ziglog/ziglog/domain/member/service/MemberService.java
@@ -1,17 +1,17 @@
 package com.ziglog.ziglog.domain.member.service;
 
 import com.ziglog.ziglog.domain.member.entity.Member;
+import com.ziglog.ziglog.domain.member.exception.exceptions.InvalidUserModificationRequestException;
+import com.ziglog.ziglog.domain.member.exception.exceptions.UserNotFoundException;
 
 public interface MemberService {
 
-    Member findUserByEmail(String email) throws Exception;
-    Member findUserByNickname(String nickname) throws Exception;
-    void modifyUserNickname(Member member, String nickname) throws Exception;
-    void modifyUserProfile(Member member, String profileUrl) throws Exception;
+    Member findUserByEmail(String email) throws UserNotFoundException;
+    Member findUserByNickname(String nickname) throws UserNotFoundException;
+    void modifyUserNickname(Member member, String nickname) throws UserNotFoundException, InvalidUserModificationRequestException;
+    void modifyUserProfile(Member member, String profileUrl) throws UserNotFoundException;
     Member signUp(String email, String nickname) throws Exception;
     boolean isValidNickname(String nickname);
     boolean isValidNicknameFormat(String nickname);
     boolean isNotDuplicatedNickname(String nickname);
-
-    void testContext(Member member) throws Exception;
 }
