e7ae38b818bb631e7612680286a5a2478548bbe4
Hanul Kim <hanulkim2000@korea.ac.kr>
2023-11-06 16:38:16 +0900

    fix: metaData 수정

--- frontend/app/(pages)/search/layout.tsx
+++ frontend/app/(pages)/search/layout.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from 'react';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: '검색 (ZigLogSearch)',
+  title: '검색 - ZigLogSearch',
   description:
     'ZigLogS에서 글을 찾아보세요. 저희 검색 페이지를 통해 빠르고 정확한 결과얻을 수 있습니다.',
 };
