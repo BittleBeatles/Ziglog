e83fee8939f58119afa0fd931542ee2c25308b25
박영서 <pj0642@gmail.com>
2023-11-07 15:42:39 +0900

    rename: alarm -> notification으로 이름 변경. - NotificationService 정의

---
+++ backend/src/main/java/com/ziglog/ziglog/global/notification/service/NotificationService.java
@@ -0,0 +1,12 @@
+package com.ziglog.ziglog.global.notification.service;
+
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+public interface NotificationService {
+
+    //SSE 연결 만들기
+    SseEmitter subscribe();
+
+    //이벤트 객체 전송
+    void notifyEvent(Long memberId, Object event);
+}
---
+++ backend/src/main/java/com/ziglog/ziglog/global/notification/service/NotificationServiceImpl.java
@@ -0,0 +1,9 @@
+package com.ziglog.ziglog.global.notification.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationServiceImpl implements NotificationService {
+
+
+}
