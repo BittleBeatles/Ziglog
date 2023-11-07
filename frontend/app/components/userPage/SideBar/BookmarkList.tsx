9018f7bd69b15b42b7f946d8fb5593c77a964f43
Hanul Kim <hanulkim2000@korea.ac.kr>
2023-11-07 16:08:35 +0900

    feat: 사이드바 북마크 리스트에서 북마크 제거 기능 구현
    - 테마 적용 완료
    - sweetalert 사용해서 해제 여부 묻고 확인받으면 해제함

--- frontend/app/components/userPage/SideBar/BookmarkList.tsx
+++ frontend/app/components/userPage/SideBar/BookmarkList.tsx
@@ -4,21 +4,53 @@ import IconButton from '@components/common/IconButton';
 import SvgIcon from '@components/common/SvgIcon';
 import Text from '@components/common/Text';
 import colors from '@src/design/color';
+import { showAlert } from '@src/util/alert';
 import Link from 'next/link';
-import { useState } from 'react';
+import { useState, useContext } from 'react';
+import Swal from 'sweetalert2';
+import { deleteBookmark } from '@api/bookmark/bookmark';
+import SideDataContext from '@(pages)/user-page/[userNickname]/SideDataContext';
+import { useRouter } from 'next/navigation';
 
 export interface BookmarkListProps {
   noteList: Note[];
   theme?: 'light' | 'dark';
 }
 
+const THEME_VARIANTS = {
+  light: '#F6F6F6',
+  dark: '#3A3A3C',
+};
+
 export default function BookmarkList({
   noteList,
   theme = 'light',
 }: BookmarkListProps) {
+  const router = useRouter();
   const [showAll, setShowAll] = useState(false);
   const displayedNotes = showAll ? noteList : noteList.slice(0, 5);
-
+  const { getBookmarkList } = useContext(SideDataContext);
+  const handleUndoBookmark = (noteId: number, theme: 'light' | 'dark') => {
+    const bg = THEME_VARIANTS[theme];
+    const textColor = theme === 'light' ? 'black' : 'white';
+    Swal.fire({
+      html: '해당 노트에 대한 북마크를<br>해제하시겠습니까?',
+      showCloseButton: true,
+      width: 300,
+      background: bg,
+      color: textColor,
+      confirmButtonText: '해제하기',
+      confirmButtonColor: '#3D4EFE',
+    }).then(async (res) => {
+      if (res.isConfirmed) {
+        const res = deleteBookmark(noteId);
+        if (await res) {
+          showAlert('성공적으로 해제되었습니다', 'success');
+          getBookmarkList();
+        }
+      }
+    });
+  };
   return (
     <div className="w-full">
       <div className="flex justify-between">
@@ -49,20 +81,28 @@ export default function BookmarkList({
       <div className="mt-5">
         {displayedNotes.map((note) => {
           return (
-            <Link
-              href={`/user-page/${note.nickname}/read-note/${note.noteId}`}
+            <div
               key={note.noteId}
+              className="flex items-center mb-3 opacity-100 "
             >
-              <div className="flex items-center mb-3 opacity-100 hover:opacity-60 transition-opacity duration-300">
+              <span className=" cursor-pointer hover:opacity-60 transition-opacity duration-300">
                 <SvgIcon
-                  name="NoteDescription"
+                  onClick={() => handleUndoBookmark(note.noteId, theme)}
+                  name="BookMarkFill"
                   color={theme === 'light' ? colors.black : colors.white}
                 />
-                <Text className={`pl-1 truncate ${THEME_VARINTS[theme]}`}>
-                  {note.title}
-                </Text>
-              </div>
-            </Link>
+              </span>
+              <span
+                onClick={() =>
+                  router.push(
+                    `/user-page/${note.nickname}/read-note/${note.noteId}`
+                  )
+                }
+                className={`pl-1 truncate ${THEME_VARINTS[theme]} cursor-pointer hover:opacity-60 transition-opacity duration-300`}
+              >
+                {note.title}
+              </span>
+            </div>
           );
         })}
       </div>
