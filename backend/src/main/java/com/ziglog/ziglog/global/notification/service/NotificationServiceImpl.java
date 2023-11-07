e8e3a9d190ddd364607bf2b812f05153b564b59b
박영서 <pj0642@gmail.com>
2023-11-07 17:47:47 +0900

    feat: NotificatioController 추가

--- backend/src/main/java/com/ziglog/ziglog/global/notification/service/NotificationServiceImpl.java
+++ backend/src/main/java/com/ziglog/ziglog/global/notification/service/NotificationServiceImpl.java
@@ -2,10 +2,14 @@ package com.ziglog.ziglog.global.notification.service;
 
 import com.ziglog.ziglog.global.notification.repository.NotificationRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.io.IOException;
+
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class NotificationServiceImpl implements NotificationService {
     private static final Long TIMEOUT = 1000 * 60 * 30L;// 30분
@@ -13,21 +17,34 @@ public class NotificationServiceImpl implements NotificationService {
 
     @Override
     public SseEmitter subscribe(Long memberId) {
-        return null;
+        SseEmitter emitter = createEmitter(memberId);
+        notifyEvent(memberId, "Event stream created");
+        return emitter;
     }
 
     @Override
     public void notifyEvent(Long memberId, Object event) {
+        SseEmitter emitter = notificationRepository.get(memberId);
+        if (emitter != null) {
+            try {
+                log.info("emitter send");
+                emitter.send(SseEmitter.event().id(String.valueOf(memberId)).name("sse").data(event));
+            } catch (Exception e){
+                notificationRepository.deleteById(memberId);
+                emitter.completeWithError(new IOException("서버로부터 이벤트를 수신할 수 없습니다."));
+            }
+        }
+        else {
+            log.info("emitter null");
+        }
 
     }
 
     private SseEmitter createEmitter(Long memberId){
+        log.info("create new Emitter");
         SseEmitter sseEmitter = new SseEmitter(TIMEOUT);
-
-
-
+        notificationRepository.put(memberId, sseEmitter);
+        log.info("put new Emitter");
+        return sseEmitter;
     }
-
-
-
 }
