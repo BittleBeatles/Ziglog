acc73f7977a6c7f53614bb80ee67477ded30ce00
박영서 <pj0642@gmail.com>
2023-11-07 17:47:47 +0900

    feat: NotificatioController 추가

--- backend/src/main/java/com/ziglog/ziglog/global/notification/controller/NotificationController.java
+++ backend/src/main/java/com/ziglog/ziglog/global/notification/controller/NotificationController.java
@@ -4,19 +4,25 @@ import com.ziglog.ziglog.global.notification.service.NotificationService;
 import com.ziglog.ziglog.global.auth.entity.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/alarm")
 public class NotificationController {
 
-    private final NotificationService alarmService;
-    @GetMapping(value = "/subscribe", produces = "text/event-steream")
-    public SseEmitter subscribe(@AuthenticationPrincipal CustomUserDetails userDetails){
-        return new SseEmitter();
+    private final NotificationService notificationService;
+    @GetMapping(value = "/subscribe/{id}", produces = "text/event-stream")
+    public SseEmitter subscribe(@PathVariable Long id){
+        return notificationService.subscribe(id);
     }
+
+    @PostMapping("/send/{id}")
+    public void sendData(@PathVariable  Long id) throws IOException {
+        notificationService.notifyEvent(id, "hello");
+    }
+
 }
--- backend/src/main/java/com/ziglog/ziglog/global/notification/service/NotificationService.java
+++ backend/src/main/java/com/ziglog/ziglog/global/notification/service/NotificationService.java
@@ -2,11 +2,13 @@ package com.ziglog.ziglog.global.notification.service;
 
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.io.IOException;
+
 public interface NotificationService {
 
     //SSE 연결 만들기
     SseEmitter subscribe(Long memberId);
 
     //이벤트 객체 전송
-    void notifyEvent(Long memberId, Object event);
+    void notifyEvent(Long memberId, Object event) throws IOException;
 }
