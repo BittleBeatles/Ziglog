dba92d136c7644288daa717af34ef90574cd8417
박영서 <pj0642@gmail.com>
2023-11-07 15:43:34 +0900

    fix: NotificationService - subscribe()에 memberId를 파라미터로 추가

--- backend/src/main/java/com/ziglog/ziglog/global/notification/service/NotificationServiceImpl.java
+++ backend/src/main/java/com/ziglog/ziglog/global/notification/service/NotificationServiceImpl.java
@@ -1,9 +1,20 @@
 package com.ziglog.ziglog.global.notification.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @Service
 public class NotificationServiceImpl implements NotificationService {
 
+
+    @Override
+    public SseEmitter subscribe(Long memberId) {
+        return null;
+    }
+
+    @Override
+    public void notifyEvent(Long memberId, Object event) {
+
+    }
 
 }
