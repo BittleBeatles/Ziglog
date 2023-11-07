8ffe547e6313a87fee4675963f87da880c25510a
Hanul Kim <hanulkim2000@korea.ac.kr>
2023-11-07 14:43:11 +0900

    feat: 글 읽기 페이지에서 닉네임 클릭 시, 해당 유저의 그래프 뷰로 이동

--- frontend/app/(pages)/user-page/[userNickname]/read-note/[noteId]/page.tsx
+++ frontend/app/(pages)/user-page/[userNickname]/read-note/[noteId]/page.tsx
@@ -100,6 +100,11 @@ export default function ReadNote() {
     setIsBookmarked(!isBookmarked);
   };
 
+  // 닉네임 클릭 시,
+  const handleNicknameClick = () => {
+    router.push(`/user-page/${data.nickname}`);
+  };
+
   const isMine = isLogin && userNickname === data.nickname;
   return (
     hasAccess && (
@@ -107,7 +112,12 @@ export default function ReadNote() {
         <div className="mx-40 my-12">
           <Text type="h1">{data.title}</Text>
           <div className="flex flex-row place-items-center my-4">
-            <Text type="b">{data.nickname}</Text>
+            <span
+              className=" cursor-pointer font-bold"
+              onClick={handleNicknameClick}
+            >
+              {data.nickname}
+            </span>
             <Text className="mx-3" type="p">
               {data.postTime && data.postTime.toLocaleString('ko-KR')}
             </Text>
