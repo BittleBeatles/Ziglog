3f64601d1edc05546e17d58376a490cca51f2661
Hanul Kim <hanulkim2000@korea.ac.kr>
2023-11-08 11:03:59 +0900

    design: 버튼 component disabled시 디자인 수정

--- frontend/app/components/common/Button.tsx
+++ frontend/app/components/common/Button.tsx
@@ -26,7 +26,13 @@ export default function Button({
     <button
       disabled={disabled}
       onClick={onClick}
-      className={`${COLOR_VARIANTS[color]} ${size} colorClass text-white w-fit h-fit px-2 py-1 rounded-full opacity-100 hover:opacity-60 transition-opacity duration-300`}
+      className={`${
+        COLOR_VARIANTS[color]
+      } ${size} font-bold colorClass text-white w-fit h-fit px-3 py-2 rounded-full opacity-100 ${
+        !disabled
+          ? 'hover:opacity-60 transition-opacity duration-300'
+          : 'opacity-60'
+      }`}
     >
       {label}
     </button>
