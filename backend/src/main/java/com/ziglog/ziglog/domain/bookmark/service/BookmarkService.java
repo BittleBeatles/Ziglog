dadc40ccfee4239ef995e7b88df341328aaaa471
박영서 <pj0642@gmail.com>
2023-11-09 15:34:09 +0900

    refactor: Bookmark 도메인을 리팩터링 - memberRepository.findByEmail() => 모두 memberRepository.findById()로 변경함 - DTO 변환을 서비스 단의 메서드로 맡김

--- backend/src/main/java/com/ziglog/ziglog/domain/bookmark/service/BookmarkService.java
+++ backend/src/main/java/com/ziglog/ziglog/domain/bookmark/service/BookmarkService.java
@@ -1,5 +1,8 @@
 package com.ziglog.ziglog.domain.bookmark.service;
 
+import com.ziglog.ziglog.domain.bookmark.dto.request.AddBookmarkRequestDto;
+import com.ziglog.ziglog.domain.bookmark.dto.response.BookmarkListDto;
+import com.ziglog.ziglog.domain.bookmark.dto.response.IsBookmarkedDto;
 import com.ziglog.ziglog.domain.bookmark.entity.Bookmark;
 import com.ziglog.ziglog.domain.bookmark.exception.exceptions.BookmarkAlreadyExistsException;
 import com.ziglog.ziglog.domain.bookmark.exception.exceptions.BookmarkNotFoundException;
@@ -12,13 +15,13 @@ import java.util.List;
 
 public interface BookmarkService {
 
+    IsBookmarkedDto checkIsBookmarked(Member member, Long noteId) throws UserNotFoundException;
+    //북마크 가져오기
+    BookmarkListDto getBookmarkedNotes(Member member) throws UserNotFoundException;
     //북마크 추가
-    void addBookmark(Member member, Long noteId) throws UserNotFoundException, NoteNotFoundException, BookmarkAlreadyExistsException;
+    void addBookmark(Member member, AddBookmarkRequestDto addBookmarkRequestDto) throws UserNotFoundException, NoteNotFoundException, BookmarkAlreadyExistsException;
     //북마크 삭제
     void deleteBookmark(Member member, Long noteId) throws NoteNotFoundException, UserNotFoundException, BookmarkNotFoundException;
-
-    //북마크 가져오기
-    List<Note> getBookmarkedNotes(Member member) throws UserNotFoundException;
     //이 글이 북마크 된 글인지 확인
-    Boolean checkIsBookmarked(Member member, Long noteId) throws UserNotFoundException;
+    Boolean isBookmarked(Member member, Long noteId) throws UserNotFoundException;
 }
