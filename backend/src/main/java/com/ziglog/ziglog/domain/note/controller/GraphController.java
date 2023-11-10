90ad2bbee2a9a1c4561d10693ff1b91286f82317
박영서 <pj0642@gmail.com>
2023-11-10 22:03:02 +0900

    feat: 그래프 탐색 기능 구현

--- backend/src/main/java/com/ziglog/ziglog/domain/note/controller/GraphController.java
+++ backend/src/main/java/com/ziglog/ziglog/domain/note/controller/GraphController.java
@@ -2,9 +2,9 @@ package com.ziglog.ziglog.domain.note.controller;
 
 import com.ziglog.ziglog.domain.note.dto.response.GraphResponseDto;
 import com.ziglog.ziglog.domain.note.service.GraphService;
-import com.ziglog.ziglog.domain.note.service.GraphServiceImpl;
 import com.ziglog.ziglog.domain.note.service.NoteService;
 import com.ziglog.ziglog.global.util.dto.ResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,9 +22,16 @@ public class GraphController {
 
     private final NoteService noteService;
     private final GraphService graphService;
-
-    @GetMapping("")
-    public ResponseDto<GraphResponseDto> getGraphByNickname(@RequestParam("nickname") String nickname) throws Exception {
+    @Operation(summary = "사용자 노트 인용 관계 그래프 조회",
+            description = "닉네임으로 사용자가 소유한 노트의 인용 관계 그래프를 탐색")
+    @GetMapping("/note")
+    public ResponseDto<GraphResponseDto> retrieveNotesOf(@RequestParam("nickname") String nickname) throws Exception {
+        return ResponseDto.of(graphService.retrieveNotesOf(nickname));
+    }
+    @Operation(summary = "사용자 폴더 구조 그래프 조회",
+            description = "닉네임으로 사용자가 소유한 폴더 및 노트의 트리 그래프를 조회")
+    @GetMapping("/folder")
+    public ResponseDto<GraphResponseDto> retrieveFolderOf(@RequestParam("nickname") String nickname) throws Exception {
         return ResponseDto.of(graphService.retrieveParentChildOnly(noteService.getRootFolder(nickname)));
     }
 }
