5693d4369a8fb58b92d8e865342f79991fa7f44d
박영서 <pj0642@gmail.com>
2023-11-10 14:39:18 +0900

    fix: 요구사항과 다른 dto 수정

--- backend/src/main/java/com/ziglog/ziglog/domain/note/dto/request/quotation/UpdateQuotationsRequestDto.java
+++ backend/src/main/java/com/ziglog/ziglog/domain/note/dto/request/quotation/UpdateQuotationsRequestDto.java
@@ -3,11 +3,10 @@ package com.ziglog.ziglog.domain.note.dto.request.quotation;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @AllArgsConstructor
 @Getter
 public class UpdateQuotationsRequestDto {
-    List<Long> quotingIds;
+    List<Long> quotingNoteIds;
 }
