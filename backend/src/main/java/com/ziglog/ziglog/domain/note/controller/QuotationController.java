ecc972832f0e7c0508cc27e5e169d48c41deb0b9
박영서 <pj0642@gmail.com>
2023-11-10 15:18:47 +0900

    fix: QuotationController에서 @PathVariable 대신 @RequestParam 애노테이션이 붙어 있던 것을 수정 - /list/{noteId} 에서 /quoting/{noteId}로 요청 URI를 변경

--- backend/src/main/java/com/ziglog/ziglog/domain/note/controller/QuotationController.java
+++ backend/src/main/java/com/ziglog/ziglog/domain/note/controller/QuotationController.java
@@ -26,15 +26,15 @@ public class QuotationController {
     @Operation(summary = "이 글과 연결된 글들의 목록을 불러 옴",
             description = "이 글을 인용하고 있거나, 이 글이 인용하고 있는 글의 저자 닉네임과 글의 제목 목록을 조회")
     @GetMapping("/list/{noteId}")
-    public ResponseDto<QuotationListResponseDto> getNotesQuoting(@RequestParam("noteId") Long noteId) throws NoteNotFoundException {
+    public ResponseDto<QuotationListResponseDto> getNotesQuoting(@PathVariable("noteId") Long noteId) throws NoteNotFoundException {
         return ResponseDto.of(quotationService.getQuotationLists(noteId));
     }
 
     @Operation(summary = "글 하단 인용 리스트로 갱신",
             description = "글 하단에 있는 인용 리스트로 현재 글의 인용 리스트를 갱신함")
-    @PutMapping("/list/{noteId}")
+    @PutMapping("/quoting/{noteId}")
     public ResponseDto<Void> updateQuotations(@AuthenticationPrincipal CustomUserDetails userDetails,
-            @RequestParam("noteId") Long noteId, @RequestBody UpdateQuotationsRequestDto requestDto) throws NoteNotFoundException {
+            @PathVariable("noteId") Long noteId, @RequestBody UpdateQuotationsRequestDto requestDto) throws NoteNotFoundException {
         quotationService.updateQuotations(userDetails.member(), noteId, requestDto);
         return ResponseDto.of(200, "success");
     }
