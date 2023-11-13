0d197a42d8bb5bda4bed69104ac789db2c390e94
Hanul Kim <hanulkim2000@korea.ac.kr>
2023-11-13 13:16:14 +0900

    fix: 메인 페이지 이미지 오른쪽 , 왼쪽 배치 구분

--- frontend/app/components/main/Description.tsx
+++ frontend/app/components/main/Description.tsx
@@ -9,6 +9,7 @@ interface DescriptionProps {
   title: string;
   content: string;
   drawLocation: string;
+  direction: string;
 }
 
 export default function Description({
@@ -17,32 +18,63 @@ export default function Description({
   pointer,
   page,
   drawLocation,
+  direction,
 }: DescriptionProps) {
   return (
     <div className="mt-20 flex justify-center mb-14">
-      <div className="flex flex-col justify-between w-1/4">
-        <div>
-          <Text type="h1">{title}</Text>
-          <Text type="p" className="mt-4 mr-3 text-xl whitespace-pre-line">
-            {content}
-          </Text>
-        </div>
-        <div className="flex justify-center">
-          <Image src={pointer} alt={'포인터'} width={300} height={385} />
-        </div>
-      </div>
-      <div className="relative">
-        <Image
-          className="rounded-lg border border-solid shadow-xl"
-          src={page}
-          alt={'페이지'}
-          width={1067}
-          height={690}
-        />
-        <div className={`absolute z-10 ${drawLocation}`}>
-          <Drawing classColor="text-main-100" />
-        </div>
-      </div>
+      {direction === 'left' ? (
+        <>
+          <div className="flex flex-col justify-between w-1/4">
+            <div>
+              <Text type="h1">{title}</Text>
+              <Text type="p" className="mt-4 mr-3 text-xl whitespace-pre-line">
+                {content}
+              </Text>
+            </div>
+            <div className="flex justify-center">
+              <Image src={pointer} alt={'포인터'} width={300} height={385} />
+            </div>
+          </div>
+          <div className="relative">
+            <Image
+              className="rounded-lg border border-solid shadow-xl"
+              src={page}
+              alt={'페이지'}
+              width={1067}
+              height={690}
+            />
+            <div className={`absolute z-10 ${drawLocation}`}>
+              <Drawing classColor="text-main-100" />
+            </div>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="relative">
+            <Image
+              className="rounded-lg border border-solid shadow-xl"
+              src={page}
+              alt={'페이지'}
+              width={1067}
+              height={690}
+            />
+            <div className={`absolute z-10 ${drawLocation}`}>
+              <Drawing classColor="text-main-100" />
+            </div>
+          </div>
+          <div className="flex flex-col justify-between w-1/4 ml-5">
+            <div>
+              <Text type="h1">{title}</Text>
+              <Text type="p" className="mt-4 mr-3 text-xl whitespace-pre-line">
+                {content}
+              </Text>
+            </div>
+            <div className="flex justify-center">
+              <Image src={pointer} alt={'포인터'} width={300} height={385} />
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
