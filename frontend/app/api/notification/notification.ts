8b2f75fe1c1747525f0513f4b4f4e22b1f905783
ababwjdgusdk <ababwjdgusdk@naver.com>
2023-11-13 17:13:40 +0900

    feat: 헤더에 토큰 추가

--- frontend/app/api/notification/notification.ts
+++ frontend/app/api/notification/notification.ts
@@ -1,5 +1,6 @@
 import { ApiSuccessResponse } from '@api/types';
 import { NotificationList } from './types';
+import { store } from '@store/store';
 import { publicFetch } from '..';
 import { showAlert } from '@src/util/alert';
 import { API_URL } from '@api/constants';
@@ -7,8 +8,13 @@ import { API_URL } from '@api/constants';
 export type NotificationApiData = ApiSuccessResponse<NotificationList>;
 
 export function getNotificationList(): Promise<NotificationList> {
+  const accessToken = store.getState().user.accessToken;
+  const grantType = store.getState().user.grantType;
   return publicFetch<NotificationApiData>(`${API_URL}/notification`, {
     method: 'GET',
+    headers: {
+      Authorization: `${grantType} ${accessToken}`,
+    },
   })
     .then((res) => {
       if (res.body.statusCode === 200) {
