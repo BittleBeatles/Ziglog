043df82102908e2683a8a5deb2708c6ec8920bb0
ababwjdgusdk <ababwjdgusdk@naver.com>
2023-11-14 17:11:33 +0900

    design: 알림 개별 삭제 아이콘 위치 변경

--- frontend/app/components/userPage/Notification/SingleNotification.tsx
+++ frontend/app/components/userPage/Notification/SingleNotification.tsx
@@ -112,74 +112,81 @@ export default function SingleNotification({
         {...rest}
         className={`shadow ${THEME_VARIANTS[theme]} ${
           !isChecked && HOVER_COLOR[theme]
-        } w-108 px-4 h-20 rounded-md flex flex-row`}
+        } w-108 px-4 h-20 rounded-md flex flex-row  place-content-between`}
       >
-        <div className="absolute top-3 right-20 mt-2 mr-2">
-          <IconButton onClick={handleDeleteClick} theme={theme} name="Close" />
-        </div>
-        <div className="grid place-content-center">
-          {/* <Link href={`/user-page/${senderNickname}`}> */}
-          <ProfileImage size={55} src={senderProfileUrl} />
-          {/* </Link> */}
-        </div>
         <div className="flex flex-row">
-          <div
-            className={`${isChecked ? 'bg-transparent' : 'bg-red-500'}
-              w-2 h-2 rounded-full bg-red-transparent mt-3 mr-2`}
-          ></div>
           <div className="grid place-content-center">
-            <p className={`text-xs ${isChecked ? 'text-gray-500' : ''}`}>
-              {dateTime instanceof Date
-                ? dateTime.toLocaleString('ko-KR', {
-                    year: '2-digit',
-                    month: '2-digit',
-                    day: '2-digit',
-                    hour: 'numeric',
-                    minute: '2-digit',
-                  })
-                : ''}
-            </p>
-            <div className="flex flex-row">
-              <p
-                className={`truncate max-w-xxs ${
-                  isChecked ? 'text-gray-400' : ''
-                }`}
-              >
-                {senderNickname}
+            {/* <Link href={`/user-page/${senderNickname}`}> */}
+            <ProfileImage size={55} src={senderProfileUrl} />
+            {/* </Link> */}
+          </div>
+          <div className="flex flex-row">
+            <div
+              className={`${isChecked ? 'bg-transparent' : 'bg-red-500'}
+              w-2 h-2 rounded-full bg-red-transparent mt-3 mr-2`}
+            ></div>
+            <div className="grid place-content-center">
+              <p className={`text-xs ${isChecked ? 'text-gray-500' : ''}`}>
+                {dateTime instanceof Date
+                  ? dateTime.toLocaleString('ko-KR', {
+                      year: '2-digit',
+                      month: '2-digit',
+                      day: '2-digit',
+                      hour: 'numeric',
+                      minute: '2-digit',
+                    })
+                  : ''}
               </p>
-              {type === 'BOOKMARK' ? (
+              <div className="flex flex-row">
                 <p
-                  className={`leading-4 py-1 ${
+                  className={`truncate max-w-xxs ${
                     isChecked ? 'text-gray-400' : ''
                   }`}
                 >
-                  {'님이 나의 게시물을 북마크했습니다.'}
+                  {senderNickname}
                 </p>
-              ) : (
+                {type === 'BOOKMARK' ? (
+                  <p
+                    className={`leading-4 py-1 ${
+                      isChecked ? 'text-gray-400' : ''
+                    }`}
+                  >
+                    {'님이 나의 게시물을 북마크했습니다.'}
+                  </p>
+                ) : (
+                  <p
+                    className={`leading-4 py-1 ${
+                      isChecked ? 'text-gray-400' : ''
+                    }`}
+                  >
+                    {'님이 나의 게시물을 인용했습니다.'}
+                  </p>
+                )}
+              </div>
+              <div className="flex flex-row">
+                <div
+                  className={`h-4 w-1 ${
+                    isChecked ? 'bg-gray-400' : 'bg-blue-300'
+                  } mr-1`}
+                ></div>
                 <p
-                  className={`leading-4 py-1 ${
+                  className={`leading-4 font-normal  ${
                     isChecked ? 'text-gray-400' : ''
                   }`}
                 >
-                  {'님이 나의 게시물을 인용했습니다.'}
+                  {title}
                 </p>
-              )}
-            </div>
-            <div className="flex flex-row">
-              <div
-                className={`h-4 w-1 ${
-                  isChecked ? 'bg-gray-400' : 'bg-blue-300'
-                } mr-1`}
-              ></div>
-              <p
-                className={`leading-4 font-normal  ${
-                  isChecked ? 'text-gray-400' : ''
-                }`}
-              >
-                {title}
-              </p>
+              </div>
             </div>
           </div>
+        </div>
+        <div className="grid justify-end flex items-start mt-3">
+          <IconButton
+            onClick={handleDeleteClick}
+            size={20}
+            theme={theme}
+            name="Close"
+          />
         </div>
       </div>
     </Link>
