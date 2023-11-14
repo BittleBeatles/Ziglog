6b3e6fca2f780c6b2e9288b17bb95b998797444c
yongseong2 <ancjs369@naver.com>
2023-11-14 17:10:07 +0900

    fix: 페이지 화면 크기 조절

--- frontend/app/(pages)/user-page/[userNickname]/layout.tsx
+++ frontend/app/(pages)/user-page/[userNickname]/layout.tsx
@@ -28,7 +28,7 @@ export default function UserLayout({ children }: { children: ReactNode }) {
             name="DoubleArrowRight"
           />
         )}
-        <div className="p-4 w-full h-screen">
+        <div className="p-4 w-5/6 h-screen">
           <Box theme={theme}>{children}</Box>
         </div>
       </SideDataProvider>
