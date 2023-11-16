962380bd2e7854d6b233b430642f24c1d08290f4
박영서 <pj0642@gmail.com>
2023-11-16 10:07:22 +0900

    fix: title이나 content가 null인 경우를 확인

--- backend/src/main/java/com/ziglog/ziglog/domain/note/service/NoteService.java
+++ backend/src/main/java/com/ziglog/ziglog/domain/note/service/NoteService.java
@@ -42,7 +42,7 @@ public interface NoteService {
 
    // 폴더
     Folder createFolder(Member member, CreateFolderRequestDto requestDto) throws FolderNotFoundException, InconsistentNoteOwnerException, UserNotFoundException;//새로운 폴더를 추가
-    Folder modifyFolder(Member member, ModifyFolderNameRequestDto requestDto) throws InconsistentFolderOwnerException, FolderNotFoundException;//폴더의 이름을 변경
+    Folder modifyFolder(Member member, ModifyFolderNameRequestDto requestDto) throws InconsistentFolderOwnerException, FolderNotFoundException, SizeLimitExceededException;//폴더의 이름을 변경
     void deleteFolder(Member member, Long folderId) throws FolderNotFoundException, UserNotFoundException,
             InconsistentFolderOwnerException, CannotRemoveRootFolderException; //해당 id의 폴더를 삭제
     Folder getRootFolder(String nickname) throws UserNotFoundException, FolderNotFoundException; //해당 사용자의 모든 디렉토리를 반환
